821a6cc0e07b2b999b0b57985ca25660d83c6707
Viktar Mikalayeu <viktoruj@gmail.com>
2023-01-28 21:19:41 +0400

    ~

--- tasks/cks/02/scripts/master.sh
+++ tasks/cks/02/scripts/master.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-kubectl apply -f https://raw.githubusercontent.com/ViktorUJ/cks/task_01/terraform/modules/k8s_self_managment/template/tasks/cks/02/task.yaml
+kubectl apply  --kubeconfig=/root/.kube/config -f https://raw.githubusercontent.com/ViktorUJ/cks/task_01/terraform/modules/k8s_self_managment/template/tasks/cks/02/task.yaml
