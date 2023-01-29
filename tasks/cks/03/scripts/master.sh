3051cce60cc962683cd7d3070d4084eec21e8a1e
Viktar Mikalayeu <viktoruj@gmail.com>
2023-01-29 20:18:36 +0400

    ~

--- tasks/cks/03/scripts/master.sh
+++ tasks/cks/03/scripts/master.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo " *** master node  "
-curl "https://raw.githubusercontent.com/ViktorUJ/cks/TASK_02/tasks/cks/03/scripts/kube-apiserver.yaml" -o "kube-apiserver.yaml"
-cp kube-apiserver.yaml /etc/kubernetes/manifests/
+#curl "https://raw.githubusercontent.com/ViktorUJ/cks/TASK_02/tasks/cks/03/scripts/kube-apiserver.yaml" -o "kube-apiserver.yaml"
+#cp kube-apiserver.yaml /etc/kubernetes/manifests/
