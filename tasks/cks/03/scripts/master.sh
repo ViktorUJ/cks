1d68e5a958dace52bacbe36487125b7bc4fa2030
Viktar Mikalayeu <viktoruj@gmail.com>
2023-01-29 20:46:32 +0400

    ~

--- tasks/cks/03/scripts/master.sh
+++ tasks/cks/03/scripts/master.sh
@@ -2,4 +2,13 @@
 echo " *** master node  "
 curl "https://raw.githubusercontent.com/ViktorUJ/cks/TASK_02/tasks/cks/03/scripts/kube-apiserver.yaml" -o "kube-apiserver.yaml"
 cp kube-apiserver.yaml /etc/kubernetes/manifests/
+sleep 30
+kubectl get node --kubeconfig=/root/.kube/config
+while test $? -gt 0
+  do
+   sleep 5
+   echo "Trying again..."
+   kubectl get node   --kubeconfig=/root/.kube/config
+  done
+date
 kubectl delete  svc kubernetes   --kubeconfig=/root/.kube/config
