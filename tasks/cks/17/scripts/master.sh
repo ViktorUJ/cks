c5229b5964b19edee5a830d283d7c570869351e1
Viktar Mikalayeu <viktoruj@gmail.com>
2023-01-31 10:29:35 +0400

    ~

--- tasks/cks/17/scripts/master.sh
+++ tasks/cks/17/scripts/master.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 echo " *** master node task 17  "
+kubectl taint nodes --all node-role.kubernetes.io/master- --kubeconfig=/root/.kube/config
+kubectl taint nodes --all node-role.kubernetes.io/control-plane-  --kubeconfig=/root/.kube/config
