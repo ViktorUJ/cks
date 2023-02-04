0417f36596ccc62e8a532d40c6015075b46758ec
Viktar Mikalayeu <viktoruj@gmail.com>
2023-02-04 15:58:15 +0400

    ~

--- tasks/cks/05/scripts/master.sh
+++ tasks/cks/05/scripts/master.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo " *** master node  task 05"
 
-curl -L https://github.com/aquasecurity/kube-bench/releases/download/v0.6.2/kube-bench_0.6.2_linux_amd64.deb -o kube-bench_0.6.2_linux_amd64.deb
-
-apt install ./kube-bench_0.6.2_linux_amd64.deb -f
+curl -L https://github.com/aquasecurity/kube-bench/releases/download/v0.6.10/kube-bench_0.6.10_linux_amd64.deb  -o kube-bench.deb
+apt install ./kube-bench.deb -f -y
--- tasks/cks/05/scripts/worker.sh
+++ tasks/cks/05/scripts/worker.sh
@@ -2,5 +2,4 @@
 echo " *** worker node task 05"
 
 curl -L https://github.com/aquasecurity/kube-bench/releases/download/v0.6.2/kube-bench_0.6.2_linux_amd64.deb -o kube-bench_0.6.2_linux_amd64.deb
-
-apt install ./kube-bench_0.6.2_linux_amd64.deb -f
+apt install ./kube-bench_0.6.2_linux_amd64.deb -f -y
