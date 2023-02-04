f285d8eea153fb6c76ce21af64ec83c00d865ff2
VIKTAR MIKALAYEU <viktoruj@gmail.com>
2023-02-04 16:24:35 +0400

    Task 05 (#10)
    add config for task 05

---
+++ tasks/cks/05/scripts/worker.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo " *** worker node task 05"
+
+curl -L https://github.com/aquasecurity/kube-bench/releases/download/v0.6.2/kube-bench_0.6.2_linux_amd64.deb -o kube-bench_0.6.2_linux_amd64.deb
+apt install ./kube-bench_0.6.2_linux_amd64.deb -f -y
