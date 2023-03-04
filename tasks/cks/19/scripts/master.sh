481dfa3ea6b6e9c43f5a570e793e97f2c71180e5
iktar ikalayeu <viktoruj@gmail.com>
2023-03-04 19:51:22 +0400

    ~

--- tasks/cks/19/scripts/master.sh
+++ tasks/cks/19/scripts/master.sh
@@ -5,3 +5,4 @@ kubectl taint nodes --all node-role.kubernetes.io/control-plane-  --kubeconfig=/
 
 kubectl create deployment nginx --image=nginx  --kubeconfig=/root/.kube/config
 
+kubectl  apply -f https://raw.githubusercontent.com/ViktorUJ/cks/task_019/tasks/cks/19/scripts/deployment.yaml  --kubeconfig=/root/.kube/config
