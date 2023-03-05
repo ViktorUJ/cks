0dcd33923d32739f8694a289e7a43536542733d0
VIKTAR MIKALAYEU <viktoruj@gmail.com>
2023-03-05 19:13:00 +0400

    12 (#23)
    add task 12

---
+++ tasks/cks/12/scripts/master.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo " *** master node task 12 "
+kubectl taint nodes --all node-role.kubernetes.io/master- --kubeconfig=/root/.kube/config
+kubectl taint nodes --all node-role.kubernetes.io/control-plane-  --kubeconfig=/root/.kube/config
+
+kubectl  apply -f https://raw.githubusercontent.com/ViktorUJ/cks/master/tasks/cks/12/scripts/task.yaml  --kubeconfig=/root/.kube/config
