de3d50da8e56de5aa22fff89a1729b59c00e5678
iktar ikalayeu <viktoruj@gmail.com>
2023-03-11 18:50:56 +0400

    ~

--- tasks/cks/02/scripts/worker.sh
+++ tasks/cks/02/scripts/worker.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 echo " *** worker node  task 02"
 echo " *** install falco"
-#https://falco.org/docs/getting-started/installation/
-curl -s https://falco.org/repo/falcosecurity-packages.asc | apt-key add -
-echo "deb https://download.falco.org/packages/deb stable main" | tee -a /etc/apt/sources.list.d/falcosecurity.list
-apt-get update -y
-apt-get  install linux-headers-$(uname -r) clang llvm   dkms make -y
-apt-get install -y falco
 
-systemctl enable  falco
-systemctl start falco
+# https://github.com/sysdiglabs/falco-workshop
+apt-get update
+apt-get -y install gpg curl
 
+curl -o install-falco.sh -s \
+  https://s3.amazonaws.com/download.draios.com/stable/install-falco
+
+bash install-falco.sh
+
+##https://falco.org/docs/getting-started/installation/
+#curl -s https://falco.org/repo/falcosecurity-packages.asc | apt-key add -
+#echo "deb https://download.falco.org/packages/deb stable main" | tee -a /etc/apt/sources.list.d/falcosecurity.list
+#apt-get update -y
+#apt-get  install linux-headers-$(uname -r) clang llvm   dkms make -y
+#apt-get install -y falco
+#
+#systemctl enable  falco
+#systemctl start falco
+#
