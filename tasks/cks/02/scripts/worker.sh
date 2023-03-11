ffaa3651dd63da506215af70e32d3706279e2ec5
iktar ikalayeu <viktoruj@gmail.com>
2023-03-11 18:38:39 +0400

    ~

--- tasks/cks/02/scripts/worker.sh
+++ tasks/cks/02/scripts/worker.sh
@@ -5,7 +5,7 @@ echo " *** install falco"
 curl -s https://falco.org/repo/falcosecurity-packages.asc | apt-key add -
 echo "deb https://download.falco.org/packages/deb stable main" | tee -a /etc/apt/sources.list.d/falcosecurity.list
 apt-get update -y
-apt-get  install linux-headers-$(uname -r) clang llvm  dialog   dkms make -y
+apt-get  install linux-headers-$(uname -r) clang llvm   dkms make -y
 apt-get install -y falco
 
 systemctl enable  falco
