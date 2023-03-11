736a4590757e451c3dfc84cc20f387280c164407
iktar ikalayeu <viktoruj@gmail.com>
2023-03-11 20:01:29 +0400

    ~

--- tasks/cks/02/scripts/worker.sh
+++ tasks/cks/02/scripts/worker.sh
@@ -4,6 +4,9 @@ echo " *** install falco"
 
 curl -s https://s3.amazonaws.com/download.draios.com/stable/install-falco | sudo bash
 
+systemctl enable  falco
+systemctl start falco
+
 # #https://falco.org/docs/getting-started/installation/
 # curl -s https://falco.org/repo/falcosecurity-packages.asc | apt-key add -
 # echo "deb https://download.falco.org/packages/deb stable main" | tee -a /etc/apt/sources.list.d/falcosecurity.list
