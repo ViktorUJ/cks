45fd2c947f55a132f8ed1f501848584634949b29
iktar ikalayeu <viktoruj@gmail.com>
2023-03-25 09:46:23 +0400

    ~

--- docker/cks/14/build.sh
+++ docker/cks/14/build.sh
@@ -1,5 +1,5 @@
-docker build  --file Dockerfile1 -t viktoruj/cks-lab:cks_14_app1 .
+docker build  --file Dockerfile1 --compress  -t viktoruj/cks-lab:cks_14_app1 .
 docker push  viktoruj/cks-lab:cks_14_app1
 
-docker build --file Dockerfile2   -t viktoruj/cks-lab:cks_14_app2 .
+docker build --file Dockerfile2 --compress  -t viktoruj/cks-lab:cks_14_app2 .
 docker push  viktoruj/cks-lab:cks_14_app2
