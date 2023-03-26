0505950922e9bc5f8acb5d4ae47de3a0132c1349
iktar ikalayeu <viktoruj@gmail.com>
2023-03-26 10:46:39 +0400

    ~

--- tasks/cks/07/scripts/master.sh
+++ tasks/cks/07/scripts/master.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 echo " *** master node  task 07"
+kubectl taint nodes --all node-role.kubernetes.io/master- --kubeconfig=/root/.kube/config
+kubectl taint nodes --all node-role.kubernetes.io/control-plane-  --kubeconfig=/root/.kube/config
 
+export KUBECONFIG=/root/.kube/config
+helm repo add gatekeeper https://open-policy-agent.github.io/gatekeeper/charts
+helm install gatekeeper/gatekeeper --name-template=gatekeeper --namespace gatekeeper-system --create-namespace
