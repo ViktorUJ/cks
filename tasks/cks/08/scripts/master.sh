930d2e3de3d9ea05e86289ac0e2a436a246a49db
VIKTAR MIKALAYEU <viktoruj@gmail.com>
2023-04-04 21:33:07 +0400

    08 04.04.2023 (#32)
    add task 08

--- tasks/cks/08/scripts/master.sh
+++ tasks/cks/08/scripts/master.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo " *** master node task 04 "
+echo " *** master node task 08 "
 kubectl taint nodes --all node-role.kubernetes.io/master- --kubeconfig=/root/.kube/config
 kubectl taint nodes --all node-role.kubernetes.io/control-plane-  --kubeconfig=/root/.kube/config
 
-kubectl  apply -f https://raw.githubusercontent.com/ViktorUJ/cks/master/tasks/cks/04/scripts/task.yaml  --kubeconfig=/root/.kube/config
+kubectl  apply -f  https://raw.githubusercontent.com/ViktorUJ/cks/master/tasks/cks/08/scripts/task.yaml  --kubeconfig=/root/.kube/config
