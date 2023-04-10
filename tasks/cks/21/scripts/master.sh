89e0700efa545d870b42bc5cb2f45920145e8766
iktar ikalayeu <viktoruj@gmail.com>
2023-04-10 19:38:18 +0400

    ~

--- tasks/cks/21/scripts/master.sh
+++ tasks/cks/21/scripts/master.sh
@@ -6,3 +6,7 @@ wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | gpg --dear
 echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main" |  tee -a /etc/apt/sources.list.d/trivy.list
 apt-get update
 apt-get install trivy -y
+
+kubectl taint nodes --all node-role.kubernetes.io/master- --kubeconfig=/root/.kube/config
+kubectl taint nodes --all node-role.kubernetes.io/control-plane-  --kubeconfig=/root/.kube/config
+
