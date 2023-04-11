f3574422efad1abed27644e7564c5dcf2602df7b
iktar ikalayeu <viktoruj@gmail.com>
2023-04-11 18:34:42 +0400

    ~

--- tasks/cks/02/scripts/worker.sh
+++ tasks/cks/02/scripts/worker.sh
@@ -2,4 +2,13 @@
 echo " *** worker node  task 02"
 
 
+curl -s https://falco.org/repo/falcosecurity-packages.asc | apt-key add -
+echo "deb https://download.falco.org/packages/deb stable main" \ | tee -a /etc/apt/sources.list.d/falcosecurity.list
+apt-get update
+
+apt search linux-headers-$(uname -r)
+apt-get -y install linux-headers-$(uname -r)
+apt-get install -y falco=0.33.1
+
+systemctl start falco
 
