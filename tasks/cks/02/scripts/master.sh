dee839fa00f0cee36a6a369e6232a0a77c8e0f3f
iktar ikalayeu <viktoruj@gmail.com>
2023-04-12 09:16:01 +0400

    ~

--- tasks/cks/02/scripts/master.sh
+++ tasks/cks/02/scripts/master.sh
@@ -3,4 +3,4 @@ echo " *** master node task 02 "
 kubectl taint nodes --all node-role.kubernetes.io/master- --kubeconfig=/root/.kube/config
 kubectl taint nodes --all node-role.kubernetes.io/control-plane-  --kubeconfig=/root/.kube/config
 
-kubectl apply  --kubeconfig=/root/.kube/config -f https://raw.githubusercontent.com/ViktorUJ/cks/mock-10.04.2023/tasks/cks/02/scripts/task.yaml
+kubectl apply  --kubeconfig=/root/.kube/config -f https://raw.githubusercontent.com/ViktorUJ/cks/master/tasks/cks/02/scripts/task.yaml
