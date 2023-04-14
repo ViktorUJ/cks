c3f1eac3d29ac5fcddb8ee9862b91b6a620d8e7d
iktar ikalayeu <viktoruj@gmail.com>
2023-04-14 12:37:33 +0400

    ~

--- tasks/cks/09/scripts/worker.sh
+++ tasks/cks/09/scripts/worker.sh
@@ -3,4 +3,4 @@ echo " *** worker node  task 09"
 
 mkdir -p /opt/course/9/
 cd /opt/course/9/
-wget https://raw.githubusercontent.com/ViktorUJ/cks/mock-14.04.2023/tasks/cks/09/scripts/profile
+wget https://raw.githubusercontent.com/ViktorUJ/cks/master/tasks/cks/09/scripts/profile
