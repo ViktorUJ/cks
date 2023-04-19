081c31d7f722ed00f42d5dd2b7bfd68eacbe49b8
iktar ikalayeu <viktoruj@gmail.com>
2023-04-19 21:54:28 +0400

    ~

--- terraform/modules/k8s_self_managment/template/master.sh
+++ terraform/modules/k8s_self_managment/template/master.sh
@@ -84,6 +84,17 @@ if [[ "$utils_enable_sh" == "true" ]] ; then
   echo 'complete -C "/usr/local/bin/aws_completer" aws'>>/root/.bashrc
   echo 'source <(helm completion bash)'>>/root/.bashrc
   echo 'source <(skaffold completion bash)'>>/root/.bashrc
+  . /etc/os-release
+  echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:testing.list
+  curl -L "https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/Release.key" | sudo apt-key add -
+  apt-get update -qq
+  apt-get -qq -y install podman cri-tools containers-common
+  rm /etc/apt/sources.list.d/devel:kubic:libcontainers:testing.list
+  cat <<EOF | sudo tee /etc/containers/registries.conf
+  [registries.search]
+  registries = ['docker.io']
+EOF
+
 fi
 
 
