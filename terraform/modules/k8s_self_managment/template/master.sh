895e72f52bff798f477fa3256d83832073b7c497
iktar ikalayeu <viktoruj@gmail.com>
2023-04-20 09:32:53 +0400

    ~

--- terraform/modules/k8s_self_managment/template/master.sh
+++ terraform/modules/k8s_self_managment/template/master.sh
@@ -85,8 +85,8 @@ if [[ "$utils_enable_sh" == "true" ]] ; then
   echo 'source <(helm completion bash)'>>/root/.bashrc
   echo 'source <(skaffold completion bash)'>>/root/.bashrc
   . /etc/os-release
-  echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:testing.list
-  curl -L "https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/Release.key" | sudo apt-key add -
+  echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_$VERSION_ID/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:testing.list
+  curl -L "https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_$VERSION_ID/Release.key" | sudo apt-key add -
   apt-get update -qq
   apt-get -qq -y install podman cri-tools containers-common
   rm /etc/apt/sources.list.d/devel:kubic:libcontainers:testing.list
