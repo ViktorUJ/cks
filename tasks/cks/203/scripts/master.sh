a9931c0c90c4cc2dcb4209a8937e9614f0556035
iktar ikalayeu <viktoruj@gmail.com>
2023-04-30 17:11:23 +0400

    ~

--- tasks/cks/203/scripts/master.sh
+++ tasks/cks/203/scripts/master.sh
@@ -4,3 +4,64 @@ kubectl taint nodes --all node-role.kubernetes.io/master- --kubeconfig=/root/.ku
 kubectl taint nodes --all node-role.kubernetes.io/control-plane-  --kubeconfig=/root/.kube/config
 
 export KUBECONFIG=/root/.kube/config
+
+
+sudo apt update
+sudo apt install golang-cfssl
+
+#create CSR to send to KubeAPI
+cat <<EOF | cfssl genkey - | cfssljson -bare server
+{
+  "hosts": [
+    "image-bouncer-webhook",
+    "image-bouncer-webhook.default.svc",
+    "image-bouncer-webhook.default.svc.cluster.local",
+    "192.168.56.10",
+    "10.96.0.0"
+  ],
+  "CN": "system:node:image-bouncer-webhook.default.pod.cluster.local",
+  "key": {
+    "algo": "ecdsa",
+    "size": 256
+  },
+  "names": [
+    {
+      "O": "system:nodes"
+    }
+  ]
+}
+EOF
+
+#create csr request
+cat <<EOF | kubectl apply -f -
+apiVersion: certificates.k8s.io/v1
+kind: CertificateSigningRequest
+metadata:
+  name: image-bouncer-webhook.default
+spec:
+  request: $(cat server.csr | base64 | tr -d '\n')
+  signerName: kubernetes.io/kubelet-serving
+  usages:
+  - digital signature
+  - key encipherment
+  - server auth
+EOF
+
+#approver cert
+kubectl certificate approve image-bouncer-webhook.default
+
+# download signed server.crt
+kubectl get csr image-bouncer-webhook.default -o jsonpath='{.status.certificate}' | base64 --decode > server.crt
+
+mkdir -p /etc/kubernetes/pki/webhook/
+
+#copy to /etc/kubernetes/pki/webhook
+cp server.crt /etc/kubernetes/pki/webhook/server.crt
+
+# create secret with signed server.crt
+kubectl create secret tls tls-image-bouncer-webhook --key server-key.pem --cert server.crt
+
+echo "127.0.0.1 image-bouncer-webhook" >> /etc/hosts
+
+
+kubectl  apply -f https://raw.githubusercontent.com/ViktorUJ/cks/mock-28-04-2023/tasks/cks/203/scripts/task.yaml
