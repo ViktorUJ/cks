19af73d7fe73921b38bc38d1671fd6ed38239bc5
iktar ikalayeu <viktoruj@gmail.com>
2023-04-30 10:51:54 +0400

    + 203

---
+++ tasks/cks/203/scripts/master.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo " *** master node  task 203"
+kubectl taint nodes --all node-role.kubernetes.io/master- --kubeconfig=/root/.kube/config
+kubectl taint nodes --all node-role.kubernetes.io/control-plane-  --kubeconfig=/root/.kube/config
+
+export KUBECONFIG=/root/.kube/config
