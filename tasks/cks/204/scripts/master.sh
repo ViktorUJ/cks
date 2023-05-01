6a0b0fe22168bf9e6465462f926987656f5668fe
iktar ikalayeu <viktoruj@gmail.com>
2023-05-01 21:19:38 +0400

    ~

--- tasks/cks/204/scripts/master.sh
+++ tasks/cks/204/scripts/master.sh
@@ -9,3 +9,11 @@ kubectl create ns $ns
 kubectl  create secret generic  database-access-1  --from-literal pass=VerryStrongPassword1  --namespace $ns
 kubectl  create secret generic  database-access-2  --from-literal pass=VerryStrongPassword2  --namespace $ns
 kubectl  create secret generic  database-access-3  --from-literal pass=VerryStrongPassword3 --namespace $ns
+
+export RELEASE=$(curl -s https://api.github.com/repos/etcd-io/etcd/releases/latest|grep tag_name | cut -d '"' -f 4)
+wget https://github.com/etcd-io/etcd/releases/download/${RELEASE}/etcd-${RELEASE}-linux-amd64.tar.gz
+tar xvf etcd-${RELEASE}-linux-amd64.tar.gz
+cd etcd-${RELEASE}-linux-amd64
+
+mv etcd etcdctl etcdutl /usr/local/bin
+echo "*** etcd = $(etcdctl version)"
