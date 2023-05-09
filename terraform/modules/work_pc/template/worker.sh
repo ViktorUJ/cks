e239b518ecd8c6aea2fd1d7bb196b3e948990544
iktar ikalayeu <viktarm@localize.city>
2023-05-09 14:37:07 +0400

    ~

--- terraform/modules/work_pc/template/worker.sh
+++ terraform/modules/work_pc/template/worker.sh
@@ -2,22 +2,42 @@
 # install util
 # prepare k8 configs
 # check connections
+function wait_cluster_ready {
+
+echo "wait cluster $1 ready"
+aws s3 ls $2
+while test $? -gt 0
+  do
+   sleep 5
+   echo "Wait master ready .Trying again..."
+   aws s3 ls $2
+  done
+date
+
+}
+
 configs_dir="/var/work/configs"
 default_configs_dir="/root/.kube"
 
-apt-get update && sudo apt-get upgrade -y
+apt-get update
 apt-get install -y  unzip apt-transport-https ca-certificates curl jq bash-completion binutils vim
 
 curl -LO https://dl.k8s.io/release/${kubectl_version}/bin/linux/amd64/kubectl
 chmod +x kubectl
 mv kubectl  /usr/bin/
 
+echo 'source /usr/share/bash-completion/bash_completion'>>/root/.bashrc
+echo 'source <(kubectl completion bash)' >> /root/.bashrc
+echo 'alias k=kubectl' >> /root/.bashrc
+echo 'complete -F __start_kubectl k' >> /root/.bashrc
+
+
 echo "*** install aws cli "
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip"  -o "awscliv2.zip" -s
 unzip awscliv2.zip >/dev/null
 ./aws/install >/dev/null
 aws --version
-
+echo 'complete -C "/usr/local/bin/aws_completer" aws'>>/root/.bashrc
 
 mkdir $configs_dir -p
 mkdir $default_configs_dir -p
@@ -28,6 +48,7 @@ for cluster in $clusters_config; do
   cluster_name=$(echo "$cluster" | cut -d'=' -f1 )
   cluster_config_url=$(echo "$cluster" | cut -d'=' -f2 )
   echo "$cluster_name   $cluster_config_url "
+  wait_cluster_ready "$cluster_name" "$cluster_config_url"
   aws s3 cp $cluster_config_url $cluster_name
   cat $cluster_name | sed -e 's/kubernetes/'$cluster_name'/g' >/var/work/configs/$cluster_name
   KUBECONFIG+="$configs_dir/$cluster_name:"
