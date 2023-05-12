dd0bd0c340693be3ad988cbd9ad4976e324c8458
iktar ikalayeu <viktoruj@gmail.com>
2023-05-12 21:16:57 +0400

    ~

--- tasks/cks/mock/01/k8s-1/scripts/master.sh
+++ tasks/cks/mock/01/k8s-1/scripts/master.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo " *** master node  mock-1  k8s-1"
-kubectl taint nodes --all node-role.kubernetes.io/master- --kubeconfig=/root/.kube/config
-kubectl taint nodes --all node-role.kubernetes.io/control-plane-  --kubeconfig=/root/.kube/config
+export KUBECONFIG=/root/.kube/config
+kubectl taint nodes --all node-role.kubernetes.io/master-
+kubectl taint nodes --all node-role.kubernetes.io/control-plane-
 
-kubectl  apply -f  https://raw.githubusercontent.com/ViktorUJ/cks/mock_12_05_2023/tasks/cks/mock/01/k8s-1/scripts/task.yaml  --kubeconfig=/root/.kube/config
+kubectl  apply -f  https://raw.githubusercontent.com/ViktorUJ/cks/mock_12_05_2023/tasks/cks/mock/01/k8s-1/scripts/task1.yaml
+kubectl  apply -f  https://raw.githubusercontent.com/ViktorUJ/cks/mock_12_05_2023/tasks/cks/mock/01/k8s-1/scripts/task2.yaml
