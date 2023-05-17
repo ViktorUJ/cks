6abe0f84016df4663cb3287a481d330d5adeb729
iktar ikalayeu <viktoruj@gmail.com>
2023-05-17 20:38:31 +0400

    ~

--- tasks/cks/mock/01/k8s-5/scripts/master.sh
+++ tasks/cks/mock/01/k8s-5/scripts/master.sh
@@ -6,3 +6,21 @@ kubectl taint nodes --all node-role.kubernetes.io/control-plane-
 
 kubectl  apply -f  https://raw.githubusercontent.com/ViktorUJ/cks/mock_12_05_2023/tasks/cks/mock/01/k8s-5/scripts/task1.yaml
 
+export RELEASE=$(curl -s https://api.github.com/repos/etcd-io/etcd/releases/latest|grep tag_name | cut -d '"' -f 4)
+wget https://github.com/etcd-io/etcd/releases/download/${RELEASE}/etcd-${RELEASE}-linux-amd64.tar.gz
+tar xvf etcd-${RELEASE}-linux-amd64.tar.gz
+cd etcd-${RELEASE}-linux-amd64
+
+mv etcd etcdctl etcdutl /usr/local/bin
+echo "*** etcd = $(etcdctl version)"
+
+
+cat > /usr/bin/etcd_read.sh <<EOF
+ETCDCTL_API=3 etcdctl \
+   --cacert=/etc/kubernetes/pki/etcd/ca.crt   \
+   --cert=/etc/kubernetes/pki/etcd/server.crt \
+   --key=/etc/kubernetes/pki/etcd/server.key  \
+   get /registry/secrets/\$1/\$2
+EOF
+
+chmod +x /usr/bin/etcd_read.sh
