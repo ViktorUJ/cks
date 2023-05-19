f219f2c990f3eabaf5b91fc6ad3b78f9d6392f1b
iktar ikalayeu <viktoruj@gmail.com>
2023-05-19 09:57:40 +0400

    ~

--- tasks/cks/mock/01/k8s-1/scripts/master.sh
+++ tasks/cks/mock/01/k8s-1/scripts/master.sh
@@ -6,4 +6,3 @@ kubectl taint nodes --all node-role.kubernetes.io/control-plane-
 
 kubectl  apply -f  https://raw.githubusercontent.com/ViktorUJ/cks/mock_12_05_2023/tasks/cks/mock/01/k8s-1/scripts/task1.yaml
 kubectl  apply -f  https://raw.githubusercontent.com/ViktorUJ/cks/mock_12_05_2023/tasks/cks/mock/01/k8s-1/scripts/task2.yaml
-kubectl  apply -f  https://raw.githubusercontent.com/ViktorUJ/cks/mock_12_05_2023/tasks/cks/mock/01/k8s-1/scripts/task5.yaml
