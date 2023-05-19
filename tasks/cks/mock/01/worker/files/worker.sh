e387a58ea64103da89bdd1bb807af00b3e020ed4
iktar ikalayeu <viktoruj@gmail.com>
2023-05-19 17:09:28 +0400

    ~

--- tasks/cks/mock/01/worker/files/worker.sh
+++ tasks/cks/mock/01/worker/files/worker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo " *** worker pc mock-1  "
 
-mkdir -p /opt/course/9/profile
-cd /opt/course/9/profile
+mkdir -p /opt/course/9/
+cd /opt/course/9/
 wget https://raw.githubusercontent.com/ViktorUJ/cks/mock_12_05_2023/tasks/cks/mock/01/worker/files/profile
