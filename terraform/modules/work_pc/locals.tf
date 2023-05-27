cab6e88bc71bb03bc2d44334040d64505d900724
iktar ikalayeu <viktoruj@gmail.com>
2023-05-27 11:16:40 +0400

    ~

--- terraform/modules/work_pc/locals.tf
+++ terraform/modules/work_pc/locals.tf
@@ -13,7 +13,7 @@ locals {
   }
 tags_all_k8_master_x = merge(local.tags_all, local.tags_k8_master)
 
-tags_all_k8_master = var.work_pc.node_type == "spot" ? tags_all_k8_master_x : ""
+tags_all_k8_master = var.work_pc.node_type == "spot" ? local.tags_all_k8_master_x : ""
 worker_pc_ip = var.work_pc.node_type == "spot" ? aws_spot_instance_request.master["enable"].public_ip : aws_instance.master["enable"].public_ip
 
 }
