d000cb521179e90986a840519969e8521e72e656
VIKTAR MIKALAYEU <viktoruj@gmail.com>
2023-06-01 21:42:19 +0400

    update worker pc bootstrap (#46)

--- tasks/cks/mock/01/worker/files/worker.sh
+++ tasks/cks/mock/01/worker/files/worker.sh
@@ -10,13 +10,3 @@ cd /var/work/14/
 wget https://raw.githubusercontent.com/ViktorUJ/cks/master/tasks/cks/mock/01/worker/files/14/Dockerfile
 chmod 777 Dockerfile
 
-. /etc/os-release
-echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_$VERSION_ID/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:testing.list
-curl -L "https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_$VERSION_ID/Release.key" | sudo apt-key add -
-apt-get update -qq
-apt-get  -y install podman cri-tools containers-common
-rm /etc/apt/sources.list.d/devel:kubic:libcontainers:testing.list
-cat <<EOF | sudo tee /etc/containers/registries.conf
-[registries.search]
-registries = ['docker.io']
-EOF
