c41d1ba5009ab0ebc78f36f914e2a1f177c2ca59
Viktar Mikalayeu <viktoruj@gmail.com>
2023-08-31 23:05:34 +0400

    ~

--- terraform/modules/eks/output.tf
+++ terraform/modules/eks/output.tf
@@ -1,6 +1,6 @@
 output "eks_iam_app_role" {
   value = aws_iam_role.eks-app-WebIdentity.name
 }
-output "eks_cluster_name" {
+output "aws_eks_cluster" {
   value = aws_eks_cluster.eks-cluster.name
 }
