2ef7bd8a80812fe6457df81e1ef8c29b4cacdde1
Viktar Mikalayeu <viktoruj@gmail.com>
2023-09-10 00:46:41 +0400

    update docker lab

--- docker/build.sh
+++ docker/build.sh
@@ -1,16 +1,3 @@
-sudo apt-get update
-sudo apt-get install -y qemu-user-static
-sudo apt-get install -y binfmt-support
-qemu-aarch64-static --version
-update-binfmts --version
-
-export DOCKER_CLI_EXPERIMENTAL=enabled
-docker buildx create --name arm-builder --node=crossplat
-
-docker buildx use arm-builder
-docker buildx inspect --bootstrap
-docker ps
-
 docker buildx build --platform linux/arm64 --load -t viktoruj/cks-lab:arm64   .
 docker buildx build --platform linux/amd64 --load -t viktoruj/cks-lab:amd64   .
 docker push viktoruj/cks-lab:arm64
@@ -18,6 +5,3 @@ docker push viktoruj/cks-lab:amd64
 docker manifest create viktoruj/cks-lab:latest viktoruj/cks-lab:arm64  viktoruj/cks-lab:amd64
 docker manifest push viktoruj/cks-lab:latest
 
-
-#docker build -t viktoruj/cks-lab:latest .
-#docker push  viktoruj/cks-lab:latest
