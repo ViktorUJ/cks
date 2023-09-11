b88808f7dc0cc30d2b7f55f633c403a8286a933d
Viktar Mikalayeu <viktoruj@gmail.com>
2023-09-12 00:50:28 +0400

    ~

--- terraform/modules/k8s_self_managment/data.tf
+++ terraform/modules/k8s_self_managment/data.tf
@@ -1,5 +1,5 @@
 data "aws_ec2_instance_type" "test" {
-  instance_type = var.node_type
+  instance_type = var.k8s_master.instance_type
 }
 output "aws_ec2_instance_type" {
   value = data.aws_ec2_instance_type.test.supported_architectures
