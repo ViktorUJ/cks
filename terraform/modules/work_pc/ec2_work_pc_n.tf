5840b619aaa08b1a060e709034263d6a73e0039e
Viktar Mikalayeu <viktoruj@gmail.com>
2023-09-18 10:28:04 +0400

    ~

--- terraform/modules/work_pc/ec2_work_pc_n.tf
+++ terraform/modules/work_pc/ec2_work_pc_n.tf
@@ -41,6 +41,7 @@ resource "aws_spot_fleet_request" "master" {
   iam_fleet_role  = aws_iam_instance_profile.server.arn
   # spot_price      = "0.005"
   target_capacity = 1
+  wait_for_fulfillment = true
 
   launch_template_config {
     launch_template_specification {
