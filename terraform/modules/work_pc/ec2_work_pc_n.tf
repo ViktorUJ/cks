de30a05c16eeed0b6830f1139888c20e3d5f6fd3
Viktar Mikalayeu <viktoruj@gmail.com>
2023-09-19 00:44:09 +0400

    ~

--- terraform/modules/work_pc/ec2_work_pc_n.tf
+++ terraform/modules/work_pc/ec2_work_pc_n.tf
@@ -116,7 +116,7 @@ resource "aws_launch_template" "master" {
 
 
 resource "aws_spot_fleet_request" "master" {
-  iam_fleet_role       = aws_iam_role.fleet_role.arn
+  iam_fleet_role       = aws_iam_role.fleet_role["enable"].arn
   target_capacity      = 1
   wait_for_fulfillment = true
 
