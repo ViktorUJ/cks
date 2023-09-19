a8e7ceaa67da2b84f5f992bfcf74b3af78adf688
Viktar Mikalayeu <viktoruj@gmail.com>
2023-09-19 23:32:12 +0400

    ~

--- terraform/modules/k8s_self_managment/ami.tf
+++ terraform/modules/k8s_self_managment/ami.tf
@@ -1,13 +1,16 @@
 data "aws_ec2_instance_type" "master" {
     instance_type = var.k8s_master.instance_type
 }
+locals {
+    master_arch=join("",data.aws_ec2_instance_type.master.supported_architectures)
+}
 data "aws_ami" "master" {
 
     most_recent = true
 
     filter {
         name   = "name"
-        values = ["ubuntu/images/hvm-ssd/ubuntu-*-${var.k8s_master.ubuntu_version}-amd64-server-*"]
+        values = ["ubuntu/images/hvm-ssd/ubuntu-*-${var.k8s_master.ubuntu_version}-${local.master_arch}-server-*"]
     }
 
     filter {
@@ -23,5 +26,5 @@ output "ami_master" {
 }
 
 output "aws_ec2_instance_type" {
-    value = join("",data.aws_ec2_instance_type.master.supported_architectures)
+    value = local.master_arch
 }
