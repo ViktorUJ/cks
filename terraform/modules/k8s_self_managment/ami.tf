ae8494762512ade53bd0ee0befa6ab346f5bda5c
Viktar Mikalayeu <viktoruj@gmail.com>
2023-09-19 23:27:24 +0400

    ~

--- terraform/modules/k8s_self_managment/ami.tf
+++ terraform/modules/k8s_self_managment/ami.tf
@@ -1,3 +1,6 @@
+data "aws_ec2_instance_type" "master" {
+    instance_type = var.k8s_master.instance_type
+}
 data "aws_ami" "master" {
 
     most_recent = true
@@ -17,4 +20,8 @@ data "aws_ami" "master" {
 
 output "ami_master" {
   value = data.aws_ami.master
+}
+
+output "aws_ec2_instance_type" {
+    value = data.aws_ec2_instance_type.master.supported_architectures
 }
