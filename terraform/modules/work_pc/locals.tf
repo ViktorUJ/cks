912c940b065f10b5ad1b764608744d045b16fdd0
Viktar Mikalayeu <viktoruj@gmail.com>
2023-09-24 12:02:57 +0400

    ~

--- terraform/modules/work_pc/locals.tf
+++ terraform/modules/work_pc/locals.tf
@@ -2,16 +2,17 @@ locals {
   subnets_az = distinct(split(",", (var.subnets_az)))
   subnets    = [for item in local.subnets_az : split("=", item)[0]]
   az         = [for item in local.subnets_az : split("=", item)[1]]
-  tags_app = {
+  tags_app   = {
     "Name"     = "${var.aws}-${var.prefix}-${var.app_name}"
     "app_name" = var.app_name
   }
-  tags_all = merge(var.tags_common, local.tags_app)
+  tags_all       = merge(var.tags_common, local.tags_app)
   tags_k8_master = {
     "k8_node_type" = "worker-pc"
     "Name"         = "${var.aws}-${var.prefix}-${var.app_name}-worker-pc"
   }
   tags_all_k8_master = var.work_pc.node_type == "spot" ? merge(local.tags_all, local.tags_k8_master) : {}
 
- worker_pc_ip = var.work_pc.node_type == "spot" ? join("",data.aws_instances.spot_fleet["enable"].public_ips) : aws_instance.master["enable"].public_ip
+  worker_pc_ip = var.work_pc.node_type == "spot" ? join("", data.aws_instances.spot_fleet["enable"].public_ips) : aws_instance.master["enable"].public_ip
+  master_ami   = var.work_pc.ami_id != "" ? var.work_pc.ami_id : data.aws_ami.master.image_id
 }
