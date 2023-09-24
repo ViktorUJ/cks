22ffdd6b8032eeb340fe9b2c639b1ab05e1ffa42
Viktar Mikalayeu <viktoruj@gmail.com>
2023-09-24 19:15:17 +0400

    ~

--- terraform/modules/k8s_self_managment/ami.tf
+++ terraform/modules/k8s_self_managment/ami.tf
@@ -25,13 +25,22 @@ data "aws_ec2_instance_type" "worker" {
   instance_type = each.value.instance_type
 }
 
+locals {
+ worker_ami_arh= {
+   for key, instance in var.k8s_worker:
+   key =>{
+     name = join("",data.aws_ec2_instance_type.master.supported_architectures) == "x86_64" ? "amd64" : "arm64"
+   }
+ }
+}
+
 data "aws_ami" "worker" {
   for_each    = var.k8s_worker
   most_recent = true
 
   filter {
     name   = "name"
-    values = join("",data.aws_ec2_instance_type.worker["${key}"].supported_architectures) == "x86_64" ? ["ubuntu/images/hvm-ssd/ubuntu-*-${each.value.ubuntu_version}-amd64-server-*" ] : ["ubuntu/images/hvm-ssd/ubuntu-*-${each.value.ubuntu_version}-arm64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-*-${each.value.ubuntu_version}-${local.worker_ami_arh["$key"].name}-server-*"]
   }
 
   filter {
