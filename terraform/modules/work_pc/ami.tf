6fc5c1d4c97c9ae9a90b0efbd5bfca22ee309fc6
Viktar Mikalayeu <viktoruj@gmail.com>
2023-09-24 12:50:24 +0400

    ~

--- terraform/modules/work_pc/ami.tf
+++ terraform/modules/work_pc/ami.tf
@@ -1,22 +1,26 @@
 data "aws_ec2_instance_type" "master" {
   instance_type = var.work_pc.instance_type
 }
-
-data "aws_ami" "master" {
-
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = [
-      "ubuntu/images/hvm-ssd/ubuntu-*-${var.work_pc.ubuntu_version}-${join("",data.aws_ec2_instance_type.master.supported_architectures)}-server-*"
-    ]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"]
+output "arch" {
+  value = join("",data.aws_ec2_instance_type.master.supported_architectures)
 }
+#
+#data "aws_ami" "master" {
+#
+#  most_recent = true
+#
+#  filter {
+#    name   = "name"
+#    values = [
+#      "ubuntu/images/hvm-ssd/ubuntu-*-${var.work_pc.ubuntu_version}-${join("",data.aws_ec2_instance_type.master.supported_architectures)}-server-*"
+#    ]
+#  }
+#
+#  filter {
+#    name   = "virtualization-type"
+#    values = ["hvm"]
+#  }
+#
+#  owners = ["099720109477"]
+#}
+#
