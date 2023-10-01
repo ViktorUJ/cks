ab48e3b13fdb6d129493d544b9fd960d71f58a54
Viktar Mikalayeu <viktoruj@gmail.com>
2023-10-01 21:30:15 +0400

    ~

--- tasks/cks/mock/01/k8s-10/scripts/master.sh
+++ tasks/cks/mock/01/k8s-10/scripts/master.sh
@@ -3,3 +3,5 @@ echo " *** master node  mock-1  k8s-10"
 export KUBECONFIG=/root/.kube/config
 kubectl taint nodes --all node-role.kubernetes.io/master-
 kubectl taint nodes --all node-role.kubernetes.io/control-plane-
+
+
--- tasks/cks/mock/01/k8s-10/scripts/worker.sh
+++ tasks/cks/mock/01/k8s-10/scripts/worker.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 echo " *** worker node mock-1  k8s-10"
 
+mkdir /var/work/ -p
+cd /var/work/
+wget https://raw.githubusercontent.com/ViktorUJ/cks/0.3.2/tasks/cks/mock/01/k8s-10/scripts/profile-nginx.json
+
+
