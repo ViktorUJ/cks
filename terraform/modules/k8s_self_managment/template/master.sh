2b18c9c4ba582eb3df75a70242e099f09a566c67
Viktar Mikalayeu <viktoruj@gmail.com>
2023-10-04 22:21:06 +0400

    ~

--- terraform/modules/k8s_self_managment/template/master.sh
+++ terraform/modules/k8s_self_managment/template/master.sh
@@ -101,3 +101,4 @@ chmod +x  task.sh
 echo "${ssh_private_key}">/home/ubuntu/.ssh/id_rsa
 chmod 600 /home/ubuntu/.ssh/id_rsa
 echo "${ssh_pub_key}">>/home/ubuntu/.ssh/authorized_keys
+chown ubuntu:ubuntu /home/ubuntu/.ssh/id_rsa
