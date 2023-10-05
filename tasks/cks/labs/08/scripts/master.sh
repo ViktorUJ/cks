c2c1e6d784da97ee2c7baaef1244da892d4eb349
Viktar Mikalayeu <viktoruj@gmail.com>
2023-10-05 18:40:00 +0400

    ~

--- tasks/cks/labs/08/scripts/master.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo " *** master node task 08 "
-kubectl taint nodes --all node-role.kubernetes.io/master- --kubeconfig=/root/.kube/config
-kubectl taint nodes --all node-role.kubernetes.io/control-plane-  --kubeconfig=/root/.kube/config
-
-kubectl  apply -f  https://raw.githubusercontent.com/ViktorUJ/cks/master/tasks/cks/08/scripts/task.yaml  --kubeconfig=/root/.kube/config
