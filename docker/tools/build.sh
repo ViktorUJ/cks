5c3509bc777dd613dd991a4f6ac7e6a987c76fa0
Viktar Mikalayeu <viktoruj@gmail.com>
2023-10-06 12:59:04 +0400

    ~

--- docker/tools/build.sh
+++ docker/tools/build.sh
@@ -1,8 +1,7 @@
-#docker buildx build --platform linux/arm64 --load -t viktoruj/tools:arm64  -f Dockerfile_ARM  .
-docker buildx build --platform linux/amd64 --load -t viktoruj/tools:amd64  -f Dockerfile_x86.
-#docker push viktoruj/tools:arm64
+docker buildx build --platform linux/arm64 --load -t viktoruj/tools:arm64  -f Dockerfile_ARM .
+docker buildx build --platform linux/amd64 --load -t viktoruj/tools:amd64  -f Dockerfile_x86 .
+docker push viktoruj/tools:arm64
 docker push viktoruj/tools:amd64
-#docker manifest create viktoruj/tools:latest viktoruj/tools:arm64  viktoruj/tools:amd64
-docker manifest create viktoruj/tools:latest  viktoruj/tools:amd64
+docker manifest create viktoruj/tools:latest viktoruj/tools:arm64  viktoruj/tools:amd64
 docker manifest push viktoruj/tools:latest
 
