39c258683195978d43f522f6e21863c014c0abd3
Viktar Mikalayeu <viktoruj@gmail.com>
2023-10-06 12:50:59 +0400

    ~

--- docker/build.sh
+++ docker/build.sh
@@ -1,7 +1,8 @@
-docker buildx build --platform linux/arm64 --load -t viktoruj/cks-lab:arm64   .
+#docker buildx build --platform linux/arm64 --load -t viktoruj/cks-lab:arm64   .
 docker buildx build --platform linux/amd64 --load -t viktoruj/cks-lab:amd64   .
 docker push viktoruj/cks-lab:arm64
 docker push viktoruj/cks-lab:amd64
-docker manifest create viktoruj/cks-lab:latest viktoruj/cks-lab:arm64  viktoruj/cks-lab:amd64
+#docker manifest create viktoruj/cks-lab:latest viktoruj/cks-lab:arm64  viktoruj/cks-lab:amd64
+docker manifest create viktoruj/cks-lab:latest  viktoruj/cks-lab:amd64
 docker manifest push viktoruj/cks-lab:latest
 
