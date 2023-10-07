20ebdb4d88310a2382f3c2d8824492301f98e5f0
Viktar Mikalayeu <viktoruj@gmail.com>
2023-10-07 21:59:05 +0400

    ~

--- tasks/cks/labs/11/k8s-1/scripts/master.sh
+++ tasks/cks/labs/11/k8s-1/scripts/master.sh
@@ -3,11 +3,22 @@ echo " *** master node  task 11"
 ns="team-green"
 kubeconfig="--kubeconfig=/root/.kube/config"
 
+acrh=$(uname -m)
 export RELEASE=$(curl -s https://api.github.com/repos/etcd-io/etcd/releases/latest|grep tag_name | cut -d '"' -f 4)
-wget https://github.com/etcd-io/etcd/releases/download/${RELEASE}/etcd-${RELEASE}-linux-amd64.tar.gz
-tar xvf etcd-${RELEASE}-linux-amd64.tar.gz
-cd etcd-${RELEASE}-linux-amd64
+case $acrh in
+x86_64)
+  etcdctl_url="https://github.com/etcd-io/etcd/releases/download/${RELEASE}/etcd-${RELEASE}-linux-amd64.tar.gz"
+;;
+aarch64)
+  etcdctl_url="https://github.com/etcd-io/etcd/releases/download/${RELEASE}/etcd-${RELEASE}-linux-arm64.tar.gz"
+;;
+esac
 
+
+wget -O etcd.tar.gz $etcdctl_url
+tar xvf etcd.tar.gz
+etcd_dir=$(ls  | grep linux | tr -d '\n')
+cd $etcd_dir
 mv etcd etcdctl etcdutl /usr/local/bin
 echo "*** etcd = $(etcdctl version)"
 
