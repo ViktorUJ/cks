9ec9bbf1e0ac8a8aa75596ae00f280c1e4cc76bc
Viktar Mikalayeu <viktoruj@gmail.com>
2023-10-30 20:46:53 +0400

    ~

--- docker/ping_pong/app/app.go
+++ docker/ping_pong/app/app.go
@@ -2,115 +2,51 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
 	"os"
-	"runtime"
 	"strings"
-	"sync/atomic"
-	"time"
-
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-var (
-	requestsPerSecond float64
-	requestsPerMinute float64
-	lastRequestTime   time.Time
-	requestsCount     uint64  // Add this line
-)
-
-// requestHandler handles all incoming HTTP requests and returns the request details as the response.
+// requestHandler processes the incoming requests and returns a response
+// containing the request method, URL, protocol, headers, and client IP.
 func requestHandler(w http.ResponseWriter, r *http.Request) {
+	// Split the RemoteAddr into IP and port. If there's an error, use the full RemoteAddr.
+	ip, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		ip = r.RemoteAddr
+	}
+
 	var response strings.Builder
+	response.WriteString(fmt.Sprintf("Client IP: %s\n", ip))
 	response.WriteString(fmt.Sprintf("Method: %s\n", r.Method))
 	response.WriteString(fmt.Sprintf("URL: %s\n", r.URL.String()))
 	response.WriteString(fmt.Sprintf("Protocol: %s\n", r.Proto))
 	response.WriteString("Headers:\n")
 
+	// Loop through the headers and append them to the response.
 	for name, headers := range r.Header {
 		for _, h := range headers {
 			response.WriteString(fmt.Sprintf("%v: %v\n", name, h))
 		}
 	}
 
-	// Update metrics
-	atomic.AddUint64(&requestsCount, 1)
-	now := time.Now()
-	elapsed := now.Sub(lastRequestTime).Seconds()
-	lastRequestTime = now
-	requestsPerSecond = 1 / elapsed
-	requestsPerMinute = requestsPerSecond * 60
-
-	// Output all headers and meta information in the response
+	// Write the response to the client.
 	fmt.Fprint(w, response.String())
 }
 
-// metricsHandler sets up the Prometheus metrics and starts the metrics server.
-func metricsHandler() {
-	requests := prometheus.NewCounterFunc(
-		prometheus.CounterOpts{
-			Name: "requests_total",
-			Help: "Total number of requests.",
-		},
-		func() float64 {
-			return float64(atomic.LoadUint64(&requestsCount))
-		},
-	)
-
-	rps := prometheus.NewGaugeFunc(
-		prometheus.GaugeOpts{
-			Name: "requests_per_second",
-			Help: "Requests per second.",
-		},
-		func() float64 {
-			return requestsPerSecond
-		},
-	)
-
-	rpm := prometheus.NewGaugeFunc(
-		prometheus.GaugeOpts{
-			Name: "requests_per_minute",
-			Help: "Requests per minute.",
-		},
-		func() float64 {
-			return requestsPerMinute
-		},
-	)
-
-	goroutines := prometheus.NewGaugeFunc(
-		prometheus.GaugeOpts{
-			Name: "goroutines",
-			Help: "Current number of goroutines.",
-		},
-		func() float64 {
-			return float64(runtime.NumGoroutine())
-		},
-	)
-
-	prometheus.MustRegister(requests, rps, rpm, goroutines)
-
-	metricPort := os.Getenv("METRIC_PORT")
-	if metricPort == "" {
-		metricPort = "2112"  // Default port for Prometheus metrics
-	}
-
-	http.Handle("/metrics", promhttp.Handler())
-	http.ListenAndServe(":" + metricPort, nil)
-}
-
-// main function sets up the main HTTP server and the metrics server.
 func main() {
+	// Register the request handler for the root path.
 	http.HandleFunc("/", requestHandler)
-	go metricsHandler()  // Start the metrics server in a separate goroutine
 
-	port := os.Getenv("SRV_PORT")
-	if port == "" {
-		fmt.Println("SRV_PORT is not set, defaulting to 8080")
-		port = "8080"
+	// Get the server port from the SRV_PORT environment variable. Default to 8080 if not set.
+	serverPort := os.Getenv("SRV_PORT")
+	if serverPort == "" {
+		serverPort = "8080"
 	}
 
-	err := http.ListenAndServe(":" + port, nil)
+	// Start the server.
+	err := http.ListenAndServe(":"+serverPort, nil)
 	if err != nil {
 		fmt.Println("Server failed:", err)
 	}
