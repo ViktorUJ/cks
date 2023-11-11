747edec834d59d8a6017516633c3c063b34143b4
Viktar Mikalayeu <viktoruj@gmail.com>
2023-11-12 00:56:51 +0400

    ~

--- tasks/ckad/mock/01/worker/files/worker.sh
+++ tasks/ckad/mock/01/worker/files/worker.sh
@@ -6,3 +6,6 @@ curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bas
 
 mkdir -p /opt/logs/ /opt/18/
 chmod a+w /opt/logs/ /opt/18/
+
+address=$(k get no -l work_type=infra_core -o json  | jq -r '.items[] | select(.kind == "Node") | .status.addresses[] | select(.type == "InternalIP") | .address')
+echo "$address ckad.local">>/etc/hosts
