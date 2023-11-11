76f39746cba6fb31ad84cfdc2083ce5f61db883e
Viktar Mikalayeu <viktoruj@gmail.com>
2023-11-11 16:18:32 +0400

    ~

--- tasks/ckad/mock/01/k8s-1/scripts/master.sh
+++ tasks/ckad/mock/01/k8s-1/scripts/master.sh
@@ -2,16 +2,16 @@
 echo " *** master node  mock-1  k8s-1"
 export KUBECONFIG=/root/.kube/config
 
-acrh=$(uname -m)
-case $acrh in
-x86_64)
-  awscli_url="https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip"
-;;
-aarch64)
-  awscli_url="https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip"
-;;
-esac
-
+#acrh=$(uname -m)
+#case $acrh in
+#x86_64)
+#  awscli_url="https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip"
+#;;
+#aarch64)
+#  awscli_url="https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip"
+#;;
+#esac
+#
 BRANCH="CKAD-mock-questions"
 
 kubectl  apply -f  https://raw.githubusercontent.com/ViktorUJ/cks/$BRANCH/tasks/ckad/mock/01/k8s-1/scripts/task4.yaml
@@ -24,8 +24,8 @@ kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/late
 kubectl -n kube-system patch deployment metrics-server --type=json \
 -p='[{"op": "add", "path": "/spec/template/spec/containers/0/args/-", "value": "--kubelet-insecure-tls"}]]'
 
-# Helm installation
-curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+## Helm installation
+#curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 
 # ingress-nginx installation
 helm upgrade --install ingress-nginx ingress-nginx \
