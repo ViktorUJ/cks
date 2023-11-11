94fb8a16604c4354a850e29aa1c86e0c1392763b
Viktar Mikalayeu <viktoruj@gmail.com>
2023-11-12 01:32:46 +0400

    ~

--- tasks/ckad/mock/01/worker/files/worker.sh
+++ tasks/ckad/mock/01/worker/files/worker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo " *** worker pc mock-1  "
+export KUBECONFIG=/root/.kube/config
 
 # Helm installation
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
@@ -7,5 +8,5 @@ curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bas
 mkdir -p /opt/logs/ /opt/18/
 chmod a+w /opt/logs/ /opt/18/
 
-address=$(k get no -l work_type=infra_core -o json  | jq -r '.items[] | select(.kind == "Node") | .status.addresses[] | select(.type == "InternalIP") | .address')
+address=$(kubectl get no -l work_type=infra_core --context cluster1-admin@cluster1 -o json  | jq -r '.items[] | select(.kind == "Node") | .status.addresses[] | select(.type == "InternalIP") | .address')
 echo "$address ckad.local">>/etc/hosts
