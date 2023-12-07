7ac4aa8ee2b4232668be824f240612a8ef750a28
Viktar Mikalayeu <viktoruj@gmail.com>
2023-12-07 23:29:40 +0400

    ~

--- terraform/modules/eks/locals.tf
+++ terraform/modules/eks/locals.tf
@@ -3,8 +3,8 @@ locals {
   ENV_ID=var.ENV_ID=="" ? "defaultId" :var.ENV_ID
   prefix_id="${local.USER_ID}_${local.ENV_ID}"
   prefix="${local.prefix_id}_${var.prefix}"
-  item_id_lock ="CMDB_${local.USER_ID}_${local.ENV_ID}_lock_${var.app_name}_${var.prefix}"
-  item_id_data="CMDB_${local.USER_ID}_${local.ENV_ID}_data_${var.app_name}_${var.prefix}"
+  item_id_lock ="CMDB_lock_${local.USER_ID}_${local.ENV_ID}_${var.app_name}_${var.prefix}"
+  item_id_data="CMDB_data_${local.USER_ID}_${local.ENV_ID}_${var.app_name}_${var.prefix}"
   subnets            = data.aws_subnet_ids.example.ids
   availability_zones = data.aws_availability_zones.available.zone_ids
 
--- terraform/modules/k8s_self_managment/locals.tf
+++ terraform/modules/k8s_self_managment/locals.tf
@@ -3,8 +3,8 @@ locals {
   ENV_ID=var.ENV_ID=="" ? "defaultId" :var.ENV_ID
   prefix_id="${local.USER_ID}_${local.ENV_ID}"
   prefix="${local.prefix_id}_${var.prefix}"
-  item_id_lock ="CMDB_${local.USER_ID}_${local.ENV_ID}_lock_${var.app_name}_${var.prefix}"
-  item_id_data="CMDB_${local.USER_ID}_${local.ENV_ID}_data_${var.app_name}_${var.prefix}"
+  item_id_lock ="CMDB_lock_${local.USER_ID}_${local.ENV_ID}_${var.app_name}_${var.prefix}"
+  item_id_data="CMDB_data_${local.USER_ID}_${local.ENV_ID}_${var.app_name}_${var.prefix}"
   subnets_az = distinct(split(",", (var.subnets_az)))
   subnets    = [for item in local.subnets_az : split("=", item)[0]]
   az         = [for item in local.subnets_az : split("=", item)[1]]
