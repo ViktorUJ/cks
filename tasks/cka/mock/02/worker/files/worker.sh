6d1eb02e33d1fb39998829333cc472bfb3bd1df1
Viktar Mikalayeu <viktoruj@gmail.com>
2024-01-10 08:51:04 -0800

    ~

--- tasks/cka/mock/02/worker/files/worker.sh
+++ tasks/cka/mock/02/worker/files/worker.sh
@@ -2,7 +2,9 @@
 echo " *** worker pc  cka mock-2  "
 
 mkdir -p /var/work/artifact/
-chmod 777 /var/work/artifact
+echo '# script fo 16 question' >/var/work/artifact/16.sh
+chmod 777 -R /var/work/artifact
+
 
 address=$(kubectl get no -l work_type=infra_core --context cluster1-admin@cluster1 -o json  | jq -r '.items[] | select(.kind == "Node") | .status.addresses[] | select(.type == "InternalIP") | .address')
 echo "$address cka.local">>/etc/hosts
