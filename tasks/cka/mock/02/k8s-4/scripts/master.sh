8ecec9ce14366f29c8c6da57e58a85d9194608c9
Viktar Mikalayeu <viktoruj@gmail.com>
2024-01-12 22:12:50 +0400

    ~

--- tasks/cka/mock/02/k8s-4/scripts/master.sh
+++ tasks/cka/mock/02/k8s-4/scripts/master.sh
@@ -4,3 +4,22 @@ export KUBECONFIG=/root/.kube/config
 
 kubectl taint nodes --all node-role.kubernetes.io/master-
 kubectl taint nodes --all node-role.kubernetes.io/control-plane-
+
+acrh=$(uname -m)
+export RELEASE=$(curl -s https://api.github.com/repos/etcd-io/etcd/releases/latest|grep tag_name | cut -d '"' -f 4)
+case $acrh in
+x86_64)
+  etcdctl_url="https://github.com/etcd-io/etcd/releases/download/${RELEASE}/etcd-${RELEASE}-linux-amd64.tar.gz"
+;;
+aarch64)
+  etcdctl_url="https://github.com/etcd-io/etcd/releases/download/${RELEASE}/etcd-${RELEASE}-linux-arm64.tar.gz"
+;;
+esac
+
+
+wget -O etcd.tar.gz $etcdctl_url
+tar xvf etcd.tar.gz
+etcd_dir=$(ls  | grep linux | tr -d '\n')
+cd $etcd_dir
+mv etcd etcdctl etcdutl /usr/local/bin
+echo "*** etcd = $(etcdctl version)"
