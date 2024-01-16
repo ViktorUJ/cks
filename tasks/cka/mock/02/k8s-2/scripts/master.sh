24f08978de528874f894c4c741e1e80a2dd240cd
Viktar Mikalayeu <viktarm@localize.city>
2024-01-16 18:23:02 +0000

    lint

--- tasks/cka/mock/02/k8s-2/scripts/master.sh
+++ tasks/cka/mock/02/k8s-2/scripts/master.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 echo " *** master node  mock-2  k8s-2"
 export KUBECONFIG=/root/.kube/config
-
--- tasks/cka/mock/02/k8s-3/scripts/master.sh
+++ tasks/cka/mock/02/k8s-3/scripts/master.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 echo " *** master node  mock-2  k8s-3"
 export KUBECONFIG=/root/.kube/config
-
