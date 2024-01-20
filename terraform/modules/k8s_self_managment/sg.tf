e34cc665d20749c5f67887d6225799ceaa25b2c4
Viktar Mikalayeu <viktoruj@gmail.com>
2024-01-20 19:24:56 +0400

    ~

--- terraform/modules/k8s_self_managment/sg.tf
+++ terraform/modules/k8s_self_managment/sg.tf
@@ -27,7 +27,12 @@ resource "aws_security_group" "servers" {
     cidr_blocks = var.k8s_master.cidrs
   }
 
-
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = data.aws_vpc.vpc.cidr_block
+  }
 
   egress {
     from_port   = 0
