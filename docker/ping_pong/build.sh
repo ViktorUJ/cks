f62384fd5756298bf9bbd52f89b00583a747a11c
Viktar Mikalayeu <viktoruj@gmail.com>
2024-01-26 19:54:53 +0400

    ~

--- docker/ping_pong/build.sh
+++ docker/ping_pong/build.sh
@@ -1,7 +1,9 @@
-#docker buildx build --platform linux/arm64 --load -t viktoruj/ping_pong:arm64   .
-docker buildx build --platform linux/amd64 --load -t viktoruj/ping_pong:amd64   .
-#docker push viktoruj/ping_pong:arm64
-#docker push viktoruj/ping_pong:amd64
-#docker manifest create viktoruj/ping_pong:latest viktoruj/ping_pong:arm64  viktoruj/ping_pong:amd64
-#docker manifest push viktoruj/ping_pong:latest
-#
+latest_commit_hash=$(git rev-parse --short HEAD)
+
+docker buildx build --platform linux/arm64 --load -t viktoruj/ping_pong:${latest_commit_hash}-arm64   .
+docker buildx build --platform linux/amd64 --load -t viktoruj/ping_pong:${latest_commit_hash}-amd64   .
+docker push viktoruj/ping_pong:${latest_commit_hash}-arm64
+docker push viktoruj/ping_pong:${latest_commit_hash}-amd64
+docker manifest create viktoruj/ping_pong:${latest_commit_hash} viktoruj/ping_pong:${latest_commit_hash}-arm64  viktoruj/ping_pong:${latest_commit_hash}-amd64
+docker manifest push viktoruj/ping_pong:${latest_commit_hash}
+
