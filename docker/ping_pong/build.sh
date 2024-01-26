389352634c871b6dac6583a6be4346fdfd100533
Viktar Mikalayeu <viktoruj@gmail.com>
2024-01-26 18:59:12 +0400

    ~

--- docker/ping_pong/build.sh
+++ docker/ping_pong/build.sh
@@ -1,6 +1,7 @@
-docker buildx build --platform linux/arm64 --load -t viktoruj/ping_pong:arm64   .
+#docker buildx build --platform linux/arm64 --load -t viktoruj/ping_pong:arm64   .
 docker buildx build --platform linux/amd64 --load -t viktoruj/ping_pong:amd64   .
-docker push viktoruj/ping_pong:arm64
-docker push viktoruj/ping_pong:amd64
-docker manifest create viktoruj/ping_pong:latest viktoruj/ping_pong:arm64  viktoruj/ping_pong:amd64
-docker manifest push viktoruj/ping_pong:latest
+#docker push viktoruj/ping_pong:arm64
+#docker push viktoruj/ping_pong:amd64
+#docker manifest create viktoruj/ping_pong:latest viktoruj/ping_pong:arm64  viktoruj/ping_pong:amd64
+#docker manifest push viktoruj/ping_pong:latest
+#
