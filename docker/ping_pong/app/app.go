de0ead8642ae3dabc52f0b9a55fcdd5d86c16c49
Viktar Mikalayeu <viktoruj@gmail.com>
2024-01-26 22:48:49 +0400

    ~

--- docker/ping_pong/app/app.go
+++ docker/ping_pong/app/app.go
@@ -133,6 +133,7 @@ func init() {
 
     time.Sleep(time.Duration(memoryUsageIncreaseTime) * time.Second)
     slice = nil
+    runtime.GC()
 
     size = memoryUsageMax * 1024 * 1024
     slice = make([]byte, size)
