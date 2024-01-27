8a8bd8e0362fceab5bfe52cb7e8f3451a3be1373
Viktar Mikalayeu <viktoruj@gmail.com>
2024-01-28 00:01:33 +0400

    ~

--- docker/ping_pong/build.sh
+++ docker/ping_pong/build.sh
@@ -14,22 +14,43 @@ while [[ $# > 0 ]]; do
     shift
 done
 
-docker buildx build --platform linux/arm64 --load -t viktoruj/ping_pong:${latest_commit_hash}-arm64   .
-docker buildx build --platform linux/amd64 --load -t viktoruj/ping_pong:${latest_commit_hash}-amd64   .
-docker push viktoruj/ping_pong:${latest_commit_hash}-arm64
-docker push viktoruj/ping_pong:${latest_commit_hash}-amd64
-docker manifest create viktoruj/ping_pong:${latest_commit_hash} viktoruj/ping_pong:${latest_commit_hash}-arm64  viktoruj/ping_pong:${latest_commit_hash}-amd64
-docker manifest push viktoruj/ping_pong:${latest_commit_hash}
+
 
 case $release in
 true)
+   docker buildx build --platform linux/arm64 --load -t viktoruj/ping_pong:${latest_commit_hash}-arm64   .
+   docker buildx build --platform linux/amd64 --load -t viktoruj/ping_pong:${latest_commit_hash}-amd64   .
+   docker push viktoruj/ping_pong:${latest_commit_hash}-arm64
+   docker push viktoruj/ping_pong:${latest_commit_hash}-amd64
+   docker manifest create viktoruj/ping_pong:${latest_commit_hash} viktoruj/ping_pong:${latest_commit_hash}-arm64  viktoruj/ping_pong:${latest_commit_hash}-amd64
+   docker manifest push viktoruj/ping_pong:${latest_commit_hash}
    echo "*** do release"
    docker manifest rm viktoruj/ping_pong:latest
    docker manifest create viktoruj/ping_pong:latest viktoruj/ping_pong:${latest_commit_hash}-arm64  viktoruj/ping_pong:${latest_commit_hash}-amd64
    docker manifest push viktoruj/ping_pong:latest
 ;;
+alpine)
+   echo "*** do release alpine"
+   docker buildx build --platform linux/arm64 --load -t viktoruj/ping_pong:${latest_commit_hash}-arm64-alpine  -f  Dockerfile_alpine .
+   docker buildx build --platform linux/amd64 --load -t viktoruj/ping_pong:${latest_commit_hash}-amd64-alpine  -f  Dockerfile_alpine  .
+   docker push viktoruj/ping_pong:${latest_commit_hash}-arm64-alpine
+   docker push viktoruj/ping_pong:${latest_commit_hash}-amd64-alpine
+   docker manifest create viktoruj/ping_pong:${latest_commit_hash}-alpine  viktoruj/ping_pong:${latest_commit_hash}-arm64-alpine   viktoruj/ping_pong:${latest_commit_hash}-amd64-alpine
+   docker manifest push viktoruj/ping_pong:${latest_commit_hash}-alpine
+
+   docker manifest rm viktoruj/ping_pong:alpine
+   docker manifest create viktoruj/ping_pong:alpine viktoruj/ping_pong:${latest_commit_hash}-arm64-alpine  viktoruj/ping_pong:${latest_commit_hash}-amd64-alpine
+   docker manifest push viktoruj/ping_pong:alpine
+
+;;
 *)
- echo "*** not need release"
+   docker buildx build --platform linux/arm64 --load -t viktoruj/ping_pong:${latest_commit_hash}-arm64   .
+   docker buildx build --platform linux/amd64 --load -t viktoruj/ping_pong:${latest_commit_hash}-amd64   .
+   docker push viktoruj/ping_pong:${latest_commit_hash}-arm64
+   docker push viktoruj/ping_pong:${latest_commit_hash}-amd64
+   docker manifest create viktoruj/ping_pong:${latest_commit_hash} viktoruj/ping_pong:${latest_commit_hash}-arm64  viktoruj/ping_pong:${latest_commit_hash}-amd64
+   docker manifest push viktoruj/ping_pong:${latest_commit_hash}
+   echo "*** not need release"
 ;;
 
 esac
