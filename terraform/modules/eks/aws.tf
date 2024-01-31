7d5eaf41944ff05324107a92ef79423ee2f4e87d
Viktar Mikalayeu <viktoruj@gmail.com>
2024-01-31 19:18:23 +0400

    ~

--- terraform/modules/eks/aws.tf
+++ terraform/modules/eks/aws.tf
@@ -2,3 +2,6 @@ provider "aws" {
   region  = var.region
   profile = var.aws
 }
+provider "aws_cmdb" {
+  region = var.region_cmdb
+}
--- terraform/modules/work_pc/providers.tf
+++ terraform/modules/work_pc/providers.tf
@@ -1,3 +1,6 @@
 provider "aws" {
   region = var.region
 }
+provider "aws_cmdb" {
+  region = var.region_cmdb
+}
