9d99e2e305d67fe1959afbbb684d9bf207a98ee1
Viktar Mikalayeu <viktoruj@gmail.com>
2024-02-03 10:18:37 +0400

    ~

--- terraform/modules/k8s_self_managment/template/master.sh
+++ terraform/modules/k8s_self_managment/template/master.sh
@@ -2,7 +2,8 @@
 ssh_password_enable_check=${ssh_password_enable}
 case $ssh_password_enable_check in
 true)
-    echo -e "${ssh_password}\n${ssh_password}" | passwd ubuntu
+  #  echo -e "${ssh_password}\n${ssh_password}" | passwd ubuntu
+    echo "ubuntu:${ssh_password}" |chpasswd
     SSH_CONFIG_FILE="/etc/ssh/sshd_config"
     sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/' $SSH_CONFIG_FILE
     sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' $SSH_CONFIG_FILE
