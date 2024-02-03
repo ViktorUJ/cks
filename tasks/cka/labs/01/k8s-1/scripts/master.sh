25449baf81259c4ce7921e1fcf9020f61d4783b6
Viktar Mikalayeu <viktoruj@gmail.com>
2024-02-03 21:38:05 +0400

    ~

--- tasks/cka/labs/01/k8s-1/scripts/master.sh
+++ tasks/cka/labs/01/k8s-1/scripts/master.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 echo " *** master node  cka lab-1  k8s-1"
 export KUBECONFIG=/root/.kube/config
+
+
+sed -i '/--advertise-address=/a \    - --new-option2=value' /etc/kubernetes/manifests/kube-apiserver.yaml
+
+sed -i 's/Pod/PoD/g'  /etc/kubernetes/manifests/kube-apiserver.yaml
+service kubelet restart
