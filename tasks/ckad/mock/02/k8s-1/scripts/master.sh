f8a784d11d59744ab24946b013de8869bfa21917
Viktar Mikalayeu <viktoruj@gmail.com>
2024-03-08 15:38:16 +0400

    ~

--- tasks/ckad/mock/02/k8s-1/scripts/master.sh
+++ tasks/ckad/mock/02/k8s-1/scripts/master.sh
@@ -31,6 +31,9 @@ kubectl  apply -f  https://raw.githubusercontent.com/ViktorUJ/cks/AG-26/tasks/ck
 # task 17
 kubectl  apply -f  https://raw.githubusercontent.com/ViktorUJ/cks/AG-26/tasks/ckad/mock/02/k8s-1/scripts/task17.yaml
 
+# task 18
+kubectl  apply -f  https://raw.githubusercontent.com/ViktorUJ/cks/AG-26/tasks/ckad/mock/02/k8s-1/scripts/task18.yaml
+
 
 # Installation of metrics server
 kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
