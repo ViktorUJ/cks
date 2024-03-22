0daaa5dcb186e106f93463cbc783b28b37214b8f
Viktar Mikalayeu <viktoruj@gmail.com>
2024-03-22 09:38:02 +0400

    ~

--- tasks/cka/labs/03/k8s-1/scripts/master.sh
+++ tasks/cka/labs/03/k8s-1/scripts/master.sh
@@ -2,8 +2,6 @@
 echo " *** master node  cka lab-2  k8s-1"
 export KUBECONFIG=/root/.kube/config
 
-kubectl  apply -f  https://raw.githubusercontent.com/ViktorUJ/cks/AG-46/tasks/cka/labs/03/k8s-1/scripts/1.yaml
-
 # Installation of metrics server
 kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
 kubectl -n kube-system patch deployment metrics-server --type=json \
@@ -18,3 +16,6 @@ helm install ingress-nginx  ingress-nginx/ingress-nginx \
   -f https://raw.githubusercontent.com/ViktorUJ/cks/AG-46/tasks/cka/labs/03/k8s-1/scripts/ingress_nginx_conf.yaml
 
 kubectl patch ingressclass nginx --patch '{"metadata": {"annotations": {"ingressclass.kubernetes.io/is-default-class": "true"}}}'
+
+
+kubectl  apply -f  https://raw.githubusercontent.com/ViktorUJ/cks/AG-46/tasks/cka/labs/03/k8s-1/scripts/1.yaml
