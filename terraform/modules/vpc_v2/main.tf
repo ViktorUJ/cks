e3be26fd3afb461385d2a57211d044153e5264ba
Viktar Mikalayeu <viktoruj@gmail.com>
2024-09-14 23:49:58 +0400

    ~

--- terraform/modules/vpc_v2/main.tf
+++ terraform/modules/vpc_v2/main.tf
@@ -2,6 +2,7 @@
 module "vpc" {
   depends_on           = [aws_dynamodb_table_item.cmdb]
   source = "ViktorUJ/vpc/aws"
+  version = "1.0.3"
   tags_default = var.tags_common
   vpc = {
     name = "${var.prefix}-${var.USER_ID}-${var.ENV_ID}-${var.STACK_NAME}-${var.STACK_TASK}"
