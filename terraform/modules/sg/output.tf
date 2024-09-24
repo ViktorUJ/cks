41d2de9068d95b4b29001fcb15383c35d2fedc5f
Viktar Mikalayeu <viktoruj@gmail.com>
2024-09-24 21:25:43 +0400

    ~

--- terraform/modules/sg/output.tf
+++ terraform/modules/sg/output.tf
@@ -1,3 +1,7 @@
-output "sg_id" {
-  value = aws_security_group.servers
+output "sg1_id" {
+  value = aws_security_group.servers1.id
+}
+
+output "sg2_id" {
+  value = aws_security_group.servers2.id
 }
