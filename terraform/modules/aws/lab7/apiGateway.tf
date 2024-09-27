f6d41748b21283fbd648c469b1635ef888d26552
Viktar Mikalayeu <viktoruj@gmail.com>
2024-09-27 23:23:16 +0400

    ~

--- terraform/modules/aws/lab7/apiGateway.tf
+++ terraform/modules/aws/lab7/apiGateway.tf
@@ -64,8 +64,30 @@ resource "aws_api_gateway_stage" "api_stage" {
   stage_name    = "prod"
 }
 
+
+# Создаем метод для корневого пути
+resource "aws_api_gateway_method" "root_method" {
+  rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
+  resource_id   = aws_api_gateway_rest_api.api_gateway.root_resource_id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+# Интеграция для корневого пути
+resource "aws_api_gateway_integration" "root_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api_gateway.id
+  resource_id             = aws_api_gateway_rest_api.api_gateway.root_resource_id
+  http_method             = aws_api_gateway_method.root_method.http_method
+  integration_http_method = "ANY"
+  type                    = "HTTP_PROXY"
+  uri                     = "http://${aws_lb.ping_pong_lb.dns_name}"  # ALB DNS имя
+}
+
+
 # Выводим URL API Gateway
 output "api_gateway_url" {
   value = "https://${aws_api_gateway_rest_api.api_gateway.id}.execute-api.${var.region}.amazonaws.com/${aws_api_gateway_stage.api_stage.stage_name}/"
   description = "The URL of the API Gateway"
 }
+
+
