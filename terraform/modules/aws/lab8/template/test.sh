53846b529c9c823fea7ae3e400f7bd2414aa0642
Viktar Mikalayeu <viktoruj@gmail.com>
2024-10-03 11:48:57 +0400

    ~

--- terraform/modules/aws/lab8/template/test.sh
+++ terraform/modules/aws/lab8/template/test.sh
@@ -18,14 +18,14 @@ make_request() {
 
 # Параметры для тестирования
 error_log="error_log.txt"  # Файл для записи ошибок
-url="http://awsgame-03-alb-344523096.eu-north-1.elb.amazonaws.com/calc?input=SGVsbG9Xb3JsZA=="  # Строка запроса
+url="https://d32wq54pes2rtl.cloudfront.net/calc?input=SGVsbG9Xb3JsZA=="  # Строка запроса
 timeout=4  # Таймаут в секундах
 
 # Очищаем файл с ошибками перед запуском
 > "$error_log"
 
 # Запускаем несколько запросов
-for i in {1..1500}; do
+for i in {1..3500}; do
     make_request $i "$url" "$error_log" "$timeout" &
 done
 
