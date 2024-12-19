b268767081845bb9b3e0781f50605f7ea07715ee
Viktar Mikalayeu <viktoruj@gmail.com>
2024-12-19 21:18:34 +0400

    ~

--- tasks/aws/labs/09/test.sh
+++ tasks/aws/labs/09/test.sh
@@ -25,3 +25,6 @@ terraform init
 curl -e "com.linkedin.android" -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36" https://github.com/ViktorUJ/terraform-aws-vpc/tree/master/examples/nacl_subnet >/dev/null
 sleep 1
 curl -e "youtube.com" -H "User-Agent: Mozilla/5.0 (Macintosh; ARM64 Mac OS X 15_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Safari/605.1.15" https://github.com/ViktorUJ/terraform-aws-vpc/blob/master/examples/simple/main.tf >/dev/null
+
+cd /tmp/
+git clone https://github.com/ViktorUJ/cks.git
