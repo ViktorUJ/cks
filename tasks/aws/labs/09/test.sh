e9837b304ad16d8f32b266425cfbfae01a4676c0
Viktar Mikalayeu <viktoruj@gmail.com>
2024-12-19 23:07:46 +0400

    ~

--- tasks/aws/labs/09/test.sh
+++ tasks/aws/labs/09/test.sh
@@ -26,5 +26,3 @@ curl -e "com.linkedin.android" -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win
 sleep 1
 curl -e "youtube.com" -H "User-Agent: Mozilla/5.0 (Macintosh; ARM64 Mac OS X 15_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Safari/605.1.15" https://github.com/ViktorUJ/terraform-aws-vpc/blob/master/examples/simple/main.tf >/dev/null
 
-cd /tmp/
-git clone https://github.com/ViktorUJ/cks.git
