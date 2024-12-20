c9e991eb6135e007f719ed9ddc56c005896381ec
Viktar Mikalayeu <viktoruj@gmail.com>
2024-12-20 13:12:23 +0400

    ~

--- tasks/aws/labs/09/test.sh
+++ tasks/aws/labs/09/test.sh
@@ -3,18 +3,82 @@ git clone https://github.com/ViktorUJ/terraform-aws-vpc.git
 cd terraform-aws-vpc/examples/simple
 terraform init
 
-curl -e "sysadminas.eu" -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36" https://github.com/ViktorUJ/terraform-aws-vpc/tree/master/examples/custom >/dev/null
+curl -X GET "https://github.com/ViktorUJ/terraform-aws-vpc/blob/master/examples/custom/main.tf" \
+  -H "Sec-Fetch-User: ?1" \
+  -H "Sec-Ch-Ua: \"Google Chrome\";v=\"131\", \"Chromium\";v=\"131\", \"Not_A Brand\";v=\"24\"" \
+  -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7" \
+  -H "Sec-Fetch-Site: none" \
+  -H "Sec-Fetch-Mode: navigate" \
+  -H "Sec-Ch-Ua-Mobile: ?0" \
+  -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36" \
+  -H "Priority: u=0, i" \
+  -H "X-Forwarded-Proto: https" \
+  -H "Upgrade-Insecure-Requests: 1" \
+  -H "Accept-Encoding: gzip, deflate, br, zstd" \
+  -H "Accept-Language: ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7" \
+  -H "Sec-Ch-Ua-Platform: \"Linux\"" \
+  -H "Sec-Fetch-Dest: document"
+
+
 sleep 1
-curl -e "https://www.google.com/" -H "User-Agent: Mozilla/5.0 (Macintosh; ARM64 Mac OS X 15_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Safari/605.1.15" https://github.com/ViktorUJ/terraform-aws-vpc/tree/master/examples/nat_gateway_subnet >/dev/null
+
+curl -X GET "https://github.com/ViktorUJ/terraform-aws-vpc/blob/master/examples/nat_gateway_subnet/main.tf" \
+  -H "X-Forwarded-Proto: https" \
+  -H "Upgrade-Insecure-Requests: 1" \
+  -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36" \
+  -H "Sec-Fetch-Mode: navigate" \
+  -H "Sec-Fetch-Dest: document" \
+  -H "Priority: u=0, i" \
+  -H "Sec-Fetch-Site: none" \
+  -H "Accept-Encoding: gzip, deflate, br, zstd" \
+  -H "Sec-Ch-Ua: \"Google Chrome\";v=\"131\", \"Chromium\";v=\"131\", \"Not_A Brand\";v=\"24\"" \
+  -H "Sec-Ch-Ua-Mobile: ?0" \
+  -H "Sec-Ch-Ua-Platform: \"Windows\"" \
+  -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7" \
+  -H "Sec-Fetch-User: ?1" \
+  -H "Accept-Language: en-US,en;q=0.9,ru;q=0.8"
+
+
 
 cd /tmp/
 git clone https://github.com/ViktorUJ/terraform-aws-vpc.git
 cd terraform-aws-vpc/examples/simple
 terraform init
 
-curl -e "linkedin.com" -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36" https://github.com/ViktorUJ/terraform-aws-vpc/tree/master/examples/nat_gateway_subnet >/dev/null
+curl -X GET "https://github.com/ViktorUJ/terraform-aws-vpc/blob/master/examples/nat_gateway_subnet/main.tf" \
+  -H "Sec-Ch-Ua: \"Google Chrome\";v=\"131\", \"Chromium\";v=\"131\", \"Not_A Brand\";v=\"24\"" \
+  -H "Sec-Ch-Ua-Platform: \"Windows\"" \
+  -H "Upgrade-Insecure-Requests: 1" \
+  -H "Sec-Fetch-Mode: navigate" \
+  -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36" \
+  -H "Sec-Fetch-Site: none" \
+  -H "Accept-Encoding: gzip, deflate, br, zstd" \
+  -H "Accept-Language: ru,ru-RU;q=0.9,en-US;q=0.8,en;q=0.7" \
+  -H "Priority: u=0, i" \
+  -H "X-Forwarded-Proto: https" \
+  -H "Sec-Ch-Ua-Mobile: ?0" \
+  -H "Sec-Fetch-User: ?1" \
+  -H "Sec-Fetch-Dest: document" \
+  -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"
+
+
 sleep 1
-curl -e "google.com" -H "User-Agent: Mozilla/5.0 (Macintosh; ARM64 Mac OS X 15_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Safari/605.1.15" https://github.com/ViktorUJ/terraform-aws-vpc/tree/master/examples/output_private_subnet_by_type >/dev/null
+
+curl -X GET "https://github.com/ViktorUJ/terraform-aws-vpc/blob/master/examples/output_private_subnet_by_type/main.tf" \
+  -H "Priority: u=0, i" \
+  -H "Sec-Fetch-Mode: navigate" \
+  -H "Sec-Ch-Ua-Platform: \"Windows\"" \
+  -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0" \
+  -H "Sec-Fetch-User: ?1" \
+  -H "Sec-Fetch-Dest: document" \
+  -H "Sec-Ch-Ua: \"Microsoft Edge\";v=\"131\", \"Chromium\";v=\"131\", \"Not_A Brand\";v=\"24\"" \
+  -H "Accept-Language: en-US,en;q=0.9" \
+  -H "Sec-Fetch-Site: none" \
+  -H "Sec-Ch-Ua-Mobile: ?0" \
+  -H "Accept-Encoding: gzip, deflate, br, zstd" \
+  -H "Upgrade-Insecure-Requests: 1" \
+  -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"
+
 
 cd /tmp/
 rm -rf terraform-aws-vpc
@@ -22,7 +86,38 @@ git clone https://github.com/ViktorUJ/terraform-aws-vpc.git
 cd terraform-aws-vpc/examples/simple
 terraform init
 
-curl -e "com.linkedin.android" -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36" https://github.com/ViktorUJ/terraform-aws-vpc/tree/master/examples/nacl_subnet >/dev/null
-sleep 1
-curl -e "youtube.com" -H "User-Agent: Mozilla/5.0 (Macintosh; ARM64 Mac OS X 15_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Safari/605.1.15" https://github.com/ViktorUJ/terraform-aws-vpc/blob/master/examples/simple/main.tf >/dev/null
 
+
+curl -X GET "https://github.com/ViktorUJ/terraform-aws-vpc/blob/master/examples/simple/main.tf" \
+  -H "Upgrade-Insecure-Requests: 1" \
+  -H "User-Agent: Mozilla/5.0 (Linux; Android 14; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.6778.135 Mobile Safari/537.36" \
+  -H "Sec-Ch-Ua: \"Android WebView\";v=\"131\", \"Chromium\";v=\"131\", \"Not_A Brand\";v=\"24\"" \
+  -H "X-Forwarded-Proto: https" \
+  -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7" \
+  -H "X-Requested-With: org.telegram.messenger" \
+  -H "Sec-Fetch-Site: none" \
+  -H "Sec-Ch-Ua-Platform: \"Android\"" \
+  -H "Sec-Ch-Ua-Mobile: ?1" \
+  -H "Accept-Encoding: gzip, deflate, br, zstd" \
+  -H "Accept-Language: ru,ru-RU;q=0.9,en-US;q=0.8,en;q=0.7" \
+  -H "Sec-Fetch-Mode: navigate" \
+  -H "Sec-Fetch-User: ?1" \
+  -H "Sec-Fetch-Dest: document" \
+  -H "Priority: u=0, i"
+
+sleep 1
+
+curl -X GET "https://github.com/ViktorUJ/terraform-aws-vpc/blob/master/examples/nacl_subnet/main.tf" \
+  -H "Sec-Fetch-Dest: document" \
+  -H "Sec-Fetch-Site: none" \
+  -H "X-Forwarded-Proto: https" \
+  -H "Sec-Ch-Ua-Mobile: ?1" \
+  -H "Priority: u=0, i" \
+  -H "Sec-Fetch-Mode: navigate" \
+  -H "User-Agent: Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Mobile Safari/537.36" \
+  -H "Sec-Ch-Ua: \"Google Chrome\";v=\"131\", \"Chromium\";v=\"131\", \"Not_A Brand\";v=\"24\"" \
+  -H "Sec-Ch-Ua-Platform: \"Android\"" \
+  -H "Upgrade-Insecure-Requests: 1" \
+  -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7" \
+  -H "Accept-Encoding: gzip, deflate, br, zstd" \
+  -H "Accept-Language: ru,ru-RU;q=0.9,en-US;q=0.8,en;q=0.7"
