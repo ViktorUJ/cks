62c585470a99a11a33a2d4d80c459ad78a20b1f6
Viktar Mikalayeu <viktoruj@gmail.com>
2025-01-29 10:03:07 +0400

    ~

--- tasks/cks/mock/02/worker/files/worker.sh
+++ tasks/cks/mock/02/worker/files/worker.sh
@@ -9,3 +9,14 @@ mkdir -p /var/work/14/
 cd /var/work/14/
 wget https://raw.githubusercontent.com/ViktorUJ/cks/AG-92/tasks/cks/mock/01/worker/files/14/Dockerfile
 chmod 777 Dockerfile
+
+sudo mkdir -p /etc/containers
+sudo tee /etc/containers/policy.json <<EOF
+{
+    "default": [
+        {
+            "type": "insecureAcceptAnything"
+        }
+    ]
+}
+EOF
