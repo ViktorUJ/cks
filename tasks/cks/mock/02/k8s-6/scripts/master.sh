9272a85f5b5a42b42b06e12073c24eb221328ddb
Viktar Mikalayeu <viktoruj@gmail.com>
2025-02-04 23:26:20 +0400

    ~

--- tasks/cks/mock/02/k8s-6/scripts/master.sh
+++ tasks/cks/mock/02/k8s-6/scripts/master.sh
@@ -4,6 +4,16 @@ export KUBECONFIG=/root/.kube/config
 kubectl taint nodes --all node-role.kubernetes.io/master-
 kubectl taint nodes --all node-role.kubernetes.io/control-plane-
 
+# ingress-nginx installation
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+
+helm install ingress-nginx  ingress-nginx/ingress-nginx \
+  --namespace ingress-nginx --create-namespace \
+  --version 4.8.3 \
+  -f https://raw.githubusercontent.com/ViktorUJ/cks/master/tasks/cka/labs/03/k8s-1/scripts/ingress_nginx_conf.yaml \
+  --wait --timeout 5m
+
+
 kubectl  apply -f  https://raw.githubusercontent.com/ViktorUJ/cks/AG-92/tasks/cks/mock/02/k8s-6/scripts/task1.yaml
 kubectl  apply -f  https://raw.githubusercontent.com/ViktorUJ/cks/AG-92/tasks/cks/mock/02/k8s-6/scripts/task5.yaml
 kubectl  apply -f  https://raw.githubusercontent.com/ViktorUJ/cks/AG-92/tasks/cks/mock/02/k8s-6/scripts/task8.yaml
