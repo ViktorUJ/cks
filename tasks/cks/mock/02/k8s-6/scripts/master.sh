df0d82a1f68c8586f2ff7f787052b818cd2f8dc2
Viktar Mikalayeu <viktoruj@gmail.com>
2025-02-04 23:28:54 +0400

    ~

--- tasks/cks/mock/02/k8s-6/scripts/master.sh
+++ tasks/cks/mock/02/k8s-6/scripts/master.sh
@@ -10,7 +10,7 @@ helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm install ingress-nginx  ingress-nginx/ingress-nginx \
   --namespace ingress-nginx --create-namespace \
   --version 4.8.3 \
-  -f https://raw.githubusercontent.com/ViktorUJ/cks/master/tasks/cka/labs/03/k8s-1/scripts/ingress_nginx_conf.yaml \
+  -f https://raw.githubusercontent.com/ViktorUJ/cks/refs/heads/AG-92/tasks/cks/mock/02/k8s-6/scripts/ingress_nginx_conf.yaml \
   --wait --timeout 5m
 
 
