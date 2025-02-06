be2ab06e1985962f82ebcd10a4ab09e6d45f7807
Viktar Mikalayeu <viktoruj@gmail.com>
2025-02-06 08:43:52 +0400

    ~

--- tasks/cks/mock/02/worker/files/worker.sh
+++ tasks/cks/mock/02/worker/files/worker.sh
@@ -58,3 +58,26 @@ chmod +r "$CERT_FILE"
 echo "Self-signed certificate created:"
 echo "  Certificate: $CERT_FILE"
 echo "  Key: $KEY_FILE"
+
+# bom install
+
+acrh=$(uname -m)
+case $acrh in
+  x86_64)
+    url="https://sre-platform.aws-guru.com/download/bom-linux-amd64"
+    ;;
+  aarch64)
+    url="https://sre-platform.aws-guru.com/download/bom-linux-arm64"
+    ;;
+esac
+
+curl -o bom -L $url
+chmod +x bom
+mv bom  /usr/bin/
+
+# trivy install
+apt-get install wget apt-transport-https gnupg lsb-release
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | gpg --dearmor | tee /usr/share/keyrings/trivy.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main" |  tee -a /etc/apt/sources.list.d/trivy.list
+apt-get update
+apt-get install trivy -y
