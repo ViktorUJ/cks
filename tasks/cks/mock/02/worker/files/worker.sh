ddb578727d952997d789cf1c8e18991ae03280e5
Viktar Mikalayeu <viktoruj@gmail.com>
2025-02-06 09:22:52 +0400

    ~

--- tasks/cks/mock/02/worker/files/worker.sh
+++ tasks/cks/mock/02/worker/files/worker.sh
@@ -81,3 +81,4 @@ wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | gpg --dear
 echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main" |  tee -a /etc/apt/sources.list.d/trivy.list
 apt-get update
 apt-get install trivy -y
+trivy image --download-db-only
