5cab56058c9113f2bad5afa5eea5f09dec0fb64c
Viktar Mikalayeu <viktoruj@gmail.com>
2025-02-18 00:09:19 +0400

    ~

--- docker/cks/mock2/12/build_app2.sh
+++ docker/cks/mock2/12/build_app2.sh
@@ -7,6 +7,6 @@ docker push viktoruj/cks-lab:${latest_commit_hash}-amd64-cks_mock2_12_app2
 docker manifest create viktoruj/cks-lab:${latest_commit_hash}-cks_mock2_12_app2  viktoruj/cks-lab:${latest_commit_hash}-arm64-cks_mock2_12_app2   viktoruj/cks-lab:${latest_commit_hash}-amd64-cks_mock2_12_app2
 docker manifest push viktoruj/cks-lab:${latest_commit_hash}-cks_mock2_12_app2
 
-docker manifest rm viktoruj/ping_pong:cks_mock2_12_app2
-docker manifest create viktoruj/ping_pong:cks_mock2_12_app2 viktoruj/ping_pong:${latest_commit_hash}-arm64-cks_mock2_12_app2  viktoruj/ping_pong:${latest_commit_hash}-amd64-cks_mock2_12_app2
-docker manifest push viktoruj/ping_pong:cks_mock2_12_app2
+docker manifest rm viktoruj/cks-lab:cks_mock2_12_app2
+docker manifest create viktoruj/cks-lab:cks_mock2_12_app2 viktoruj/cks-lab:${latest_commit_hash}-arm64-cks_mock2_12_app2  viktoruj/cks-lab:${latest_commit_hash}-amd64-cks_mock2_12_app2
+docker manifest push viktoruj/cks-lab:cks_mock2_12_app2
