16a922a515c1a6c490e2b2f356f2f9b0b83ef932
Vitali Andrushkevich <andrushkevich.vitaliy@gmail.com>
2025-03-18 15:33:37 +0500

    upd

--- tasks/cks/labs/27/k8s-1/scripts/master.sh
+++ tasks/cks/labs/27/k8s-1/scripts/master.sh
@@ -7,9 +7,14 @@ kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/late
 kubectl -n kube-system patch deployment metrics-server --type=json \
 -p='[{"op": "add", "path": "/spec/template/spec/containers/0/args/-", "value": "--kubelet-insecure-tls"}]]'
 
+#Untaint master node
+kubectl taint nodes $(hostname) node-role.kubernetes.io/control-plane:NoSchedule-
+
 # Installation of the ingress-nginx
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.12.0/deploy/static/provider/cloud/deploy.yaml
 
 kubectl apply -f  https://raw.githubusercontent.com/ViktorUJ/cks/refs/heads/master/tasks/cks/labs/27/k8s-1/scripts/app.yaml
 
-echo "myapp.local 127.0.0.1" >> /etc/hosts
+kubectl patch svc ingress-nginx-controller -n ingress-nginx --type='json' -p='[{"op": "replace", "path": "/spec/type", "value": "NodePort"}, {"op": "add", "path": "/spec/ports/0/nodePort", "value": 30800}]'
+
+echo "127.0.0.1 myapp.local" >> /etc/hosts
