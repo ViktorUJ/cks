416c0a773d04255239546c037bb738ef9723a147
Viktar Mikalayeu <viktoruj@gmail.com>
2025-04-11 09:22:42 +0400

    ~

--- tasks/cka/labs/09/k8s-1/scripts/master.sh
+++ tasks/cka/labs/09/k8s-1/scripts/master.sh
@@ -9,18 +9,20 @@ kubectl -n kube-system patch deployment metrics-server --type=json \
 
 
 #### install NGINX Gateway Fabric
-#
-## Install the Gateway API resources
-#kubectl kustomize "https://github.com/nginx/nginx-gateway-fabric/config/crd/gateway-api/standard?ref=v1.5.1" | kubectl apply -f -
-#
-## Deploy the NGINX Gateway Fabric CRDs
-#kubectl apply -f https://raw.githubusercontent.com/nginx/nginx-gateway-fabric/v1.6.1/deploy/crds.yaml
-#
-##Deploy NGINX Gateway Fabric
-#kubectl apply -f https://raw.githubusercontent.com/nginx/nginx-gateway-fabric/v1.6.1/deploy/nodeport/deploy.yaml
-#
-#kubectl patch svc nginx-gateway -n nginx-gateway --type='json' -p='[{"op": "replace", "path": "/spec/ports/0/nodePort", "value": 30080},{"op": "replace", "path": "/spec/ports/1/nodePort", "value": 30081}]'
-#
+# https://docs.nginx.com/nginx-gateway-fabric/installation/installing-ngf/manifests/
+
+
+# Install the Gateway API resources
+kubectl kustomize "https://github.com/nginx/nginx-gateway-fabric/config/crd/gateway-api/standard?ref=v1.5.1" | kubectl apply -f -
+
+# Deploy the NGINX Gateway Fabric CRDs
+kubectl apply -f https://raw.githubusercontent.com/nginx/nginx-gateway-fabric/v1.6.1/deploy/crds.yaml
+
+#Deploy NGINX Gateway Fabric
+kubectl apply -f https://raw.githubusercontent.com/nginx/nginx-gateway-fabric/v1.6.1/deploy/nodeport/deploy.yaml
+
+kubectl patch svc nginx-gateway -n nginx-gateway --type='json' -p='[{"op": "replace", "path": "/spec/ports/0/nodePort", "value": 30080},{"op": "replace", "path": "/spec/ports/1/nodePort", "value": 30081}]'
+
 
 #install lab manifests
 kubectl  apply -f  https://raw.githubusercontent.com/ViktorUJ/cks/refs/heads/AG-105/tasks/cka/labs/09/k8s-1/scripts/app.yaml
