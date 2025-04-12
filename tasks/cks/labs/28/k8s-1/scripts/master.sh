7520e5017cb160515ebadb7069bfd4a1309cd5a9
Vitali Andrushkevich <andrushkevich.vitaliy@gmail.com>
2025-04-12 15:02:43 +0500

    upd

--- tasks/cks/labs/28/k8s-1/scripts/master.sh
+++ tasks/cks/labs/28/k8s-1/scripts/master.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-echo " *** master node  cks lab-28  k8s-1"
+echo " *** master node cks lab-28 k8s-1"
 export KUBECONFIG=/root/.kube/config
 
-apt-get install -y falco
+curl -fsSL https://falco.org/repo/falcosecurity-packages.asc | gpg --dearmor -o /usr/share/keyrings/falco-archive-keyring.gpg
 
-kubectl  apply -f  https://raw.githubusercontent.com/ViktorUJ/cks/refs/heads/cks-new-labs-added/tasks/cks/labs/28/k8s-1/scripts/app.yaml
+echo "deb [signed-by=/usr/share/keyrings/falco-archive-keyring.gpg] https://download.falco.org/packages/deb stable main" | tee -a /etc/apt/sources.list.d/falcosecurity.list
+
+apt-get update -y
+
+DEBIAN_FRONTEND=noninteractive apt install -y dkms make linux-headers-$(uname -r) clang llvm dialog falco
+
+kubectl apply -f  https://raw.githubusercontent.com/ViktorUJ/cks/refs/heads/cks-new-labs-added/tasks/cks/labs/28/k8s-1/scripts/app.yaml
