3bde1271b42629139cbd4b6cf022070a1b28a894
Viktar Mikalayeu <viktoruj@gmail.com>
2025-04-13 14:48:46 +0400

    ~

--- tasks/cka/labs/09/worker/files/worker.sh
+++ tasks/cka/labs/09/worker/files/worker.sh
@@ -5,3 +5,4 @@ export KUBECONFIG=/root/.kube/config
 address=$(kubectl get no -l work_type=system --context cluster1-admin@cluster1 -o json  | jq -r '.items[] | select(.kind == "Node") | .status.addresses[] | select(.type == "InternalIP") | .address')
 echo "$address cka.local">>/etc/hosts
 echo "$address dev-cka.local">>/etc/hosts
+echo "$address weight-cka.local">>/etc/hosts
