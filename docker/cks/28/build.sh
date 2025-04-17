756f5ca6927098d9b3134d80862a7d3ba7911023
Viktar Mikalayeu <viktoruj@gmail.com>
2025-04-17 11:11:59 +0400

    ~

--- docker/cks/28/build.sh
+++ docker/cks/28/build.sh
@@ -1,3 +1,13 @@
+docker buildx build --platform linux/amd64 --load -t viktoruj/cks-lab:cks_28_app1_x86  -f  Dockerfile1  .
+docker buildx build --platform linux/arm64 --load -t viktoruj/cks-lab:cks_28_app1_arm  -f  Dockerfile1 .
+
+exit 0
+docker buildx build --platform linux/arm64 --load -t viktoruj/cks-lab:cks_28_app1_arm  -f  Dockerfile1 .
+docker buildx build --platform linux/amd64 --load -t viktoruj/cks-lab:cks_28_app1_x86  -f  Dockerfile1  .
+docker manifest create viktoruj/cks-lab:cks_28_app1    viktoruj/cks-lab:cks_28_app1_arm viktoruj/cks-lab:cks_28_app1_x86
+docker manifest push viktoruj/cks-lab:cks_28_app1
+
+exit 0
 docker build  --file Dockerfile1 --compress --no-cache -t viktoruj/cks-lab:cks_28_app1 .
 docker push viktoruj/cks-lab:cks_28_app1
 
