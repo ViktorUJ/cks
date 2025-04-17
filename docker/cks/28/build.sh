1c5ab4ff7629053af1aaa77940ee016f20e37f47
Viktar Mikalayeu <viktoruj@gmail.com>
2025-04-17 12:24:52 +0400

    ~

--- docker/cks/28/build.sh
+++ docker/cks/28/build.sh
@@ -3,12 +3,14 @@ docker buildx build --platform linux/amd64 --load -t viktoruj/cks-lab:cks_28_app
 docker manifest create viktoruj/cks-lab:cks_28_app1  viktoruj/cks-lab:cks_28_app1_arm viktoruj/cks-lab:cks_28_app1_x86
 docker manifest push viktoruj/cks-lab:cks_28_app1
 
-exit 0
-docker build  --file Dockerfile1 --compress --no-cache -t viktoruj/cks-lab:cks_28_app1 .
-docker push viktoruj/cks-lab:cks_28_app1
 
-docker build --file Dockerfile1 --compress --no-cache -t viktoruj/cks-lab:cks_28_app2 .
-docker push viktoruj/cks-lab:cks_28_app2
+docker buildx build --platform linux/arm64 --load -t viktoruj/cks-lab:cks_28_app2_arm  -f  Dockerfile1 .
+docker buildx build --platform linux/amd64 --load -t viktoruj/cks-lab:cks_28_app2_x86  -f  Dockerfile1  .
+docker manifest create viktoruj/cks-lab:cks_28_app2  viktoruj/cks-lab:cks_28_app2_arm viktoruj/cks-lab:cks_28_app2_x86
+docker manifest push viktoruj/cks-lab:cks_28_app2
 
-docker build --file Dockerfile2 --compress --no-cache -t viktoruj/cks-lab:cks_28_app3 .
-docker push viktoruj/cks-lab:cks_28_app3
+docker buildx build --platform linux/arm64 --load -t viktoruj/cks-lab:cks_28_app3_arm  -f  Dockerfile2 .
+docker buildx build --platform linux/amd64 --load -t viktoruj/cks-lab:cks_28_app3_x86  -f  Dockerfile2  .
+docker manifest create viktoruj/cks-lab:cks_28_app3  viktoruj/cks-lab:cks_28_app3_arm viktoruj/cks-lab:cks_28_app3_x86
+docker manifest push viktoruj/cks-lab:cks_28_app3
+3
