eb11bde3ae3d5081c004219ae7c7c62c65d344f8
Viktar Mikalayeu <viktoruj@gmail.com>
2025-10-01 08:27:18 +0400

    ~

--- tasks/cks/mock/04/k8s-6/scripts/master.sh
+++ tasks/cks/mock/04/k8s-6/scripts/master.sh
@@ -13,6 +13,12 @@ helm install ingress-nginx  ingress-nginx/ingress-nginx \
   -f https://raw.githubusercontent.com/ViktorUJ/cks/refs/heads/AG-120/tasks/cks/mock/04/k8s-6/scripts/ingress_nginx_conf.yaml \
   --wait --timeout 5m
 
+# Installation of metrics server
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+kubectl -n kube-system patch deployment metrics-server --type=json \
+-p='[{"op": "add", "path": "/spec/template/spec/containers/0/args/-", "value": "--kubelet-insecure-tls"}]]'
+
+
 
 kubectl  apply -f  https://raw.githubusercontent.com/ViktorUJ/cks/AG-120/tasks/cks/mock/04/k8s-6/scripts/task4.yaml
 kubectl  apply -f  https://raw.githubusercontent.com/ViktorUJ/cks/AG-120/tasks/cks/mock/04/k8s-6/scripts/task5.yaml
