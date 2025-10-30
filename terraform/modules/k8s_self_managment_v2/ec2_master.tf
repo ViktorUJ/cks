72990876e8e3ad5ac6f5ee682162bd090f379f4e
Viktar Mikalayeu <viktoruj@gmail.com>
2025-10-30 11:07:39 +0400

    ~

--- terraform/modules/k8s_self_managment_v2/ec2_master.tf
+++ terraform/modules/k8s_self_managment_v2/ec2_master.tf
@@ -94,6 +94,7 @@ resource "aws_launch_template" "master" {
       k8s_config          = local.k8s_config
       external_ip         = local.external_ip
       k8_version          = var.k8s_master.k8_version
+      k8_version_sh       = var.k8s_master.k8_version
       runtime             = var.k8s_master.runtime
       utils_enable        = var.k8s_master.utils_enable
       pod_network_cidr    = var.k8s_master.pod_network_cidr
