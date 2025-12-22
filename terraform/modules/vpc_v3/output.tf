e2f9f681d5d96e2be2f4b8dd780fdc97cfef2118
Viktar Mikalayeu <viktoruj@gmail.com>
2025-12-22 12:15:37 +0400

    ~

--- terraform/modules/vpc_v3/output.tf
+++ terraform/modules/vpc_v3/output.tf
@@ -18,6 +18,87 @@ output "vpc_default_cidr" {
   value = var.vpc_default_cidr
 }
 
+#
+#subnets public
+
+output "normalized_public_subnets_all" {
+  value = module.vpc.normalized_public_subnets_all
+}
+
+output "subnets_public_raw" {
+  value = module.vpc.subnets_public_raw
+}
+output "public_subnets_by_type" {
+  value = module.vpc.public_subnets_by_type
+}
+
+output "public_subnets_by_az" {
+  value = module.vpc.public_subnets_by_az
+}
+
+output "public_subnets_by_az_id" {
+  value = module.vpc.public_subnets_by_az_id
+}
+#subnets private
+output "normalized_private_subnets_all" {
+  value = module.vpc.normalized_private_subnets_all
+}
+
 output "subnets_private_raw" {
-  value = module.vpc.subnets_private_raw
+  value =module.vpc.subnets_private_raw
+}
+
+output "private_subnets_by_type" {
+  value = module.vpc.private_subnets_by_type
+}
+
+output "private_subnets_by_az" {
+  value = module.vpc.private_subnets_by_az
+}
+
+output "private_subnets_by_az_id" {
+  value = module.vpc.private_subnets_by_az_id
+}
+
+# NACL
+output "nacl_default_rules_raw" {
+  value = module.vpc.nacl_default_rules_raw"
+}
+output "public_nacl_raw" {
+  value = module.vpc.public_nacl_raw
+
+}
+output "public_nacl_rules_raw" {
+  value = module.vpc.public_nacl_rules_raw
+}
+
+
+output "private_nacl_raw" {
+  value = module.vpc.private_nacl_raw
+}
+output "private_nacl_rules_raw" {
+  value = module.vpc.private_nacl_rules_raw
+}
+
+# NAT Gateway
+
+output "nat_gateway_single_raw" {
+  value = module.vpc.nat_gateway_single_raw
+}
+
+output "nat_gateway_subnet_raw" {
+  value = module.vpc.nat_gateway_subnet_raw
+}
+
+output "nat_gateway_az_raw" {
+  value = module.vpc.nat_gateway_az_raw
+}
+
+# Route Table
+output "route_table_private_raw" {
+  value = module.vpc.route_table_private_raw
+}
+
+output "route_table_public_raw" {
+  value = module.vpc.route_table_public_raw
 }
