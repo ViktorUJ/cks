163c99fcf4eb354c738127bb69beb8f258943c5a
Viktar Mikalayeu <viktoruj@gmail.com>
2025-12-23 14:44:38 +0400

    ~

--- terraform/modules/eks_v2_addons/addon.tf
+++ terraform/modules/eks_v2_addons/addon.tf
@@ -5,10 +5,9 @@ resource "aws_eks_addon" "addons" {
   addon_version     = each.value.version
   resolve_conflicts_on_create = each.value.resolve_conflicts
   resolve_conflicts_on_update=each.value.resolve_conflicts
-  dynamic "configuration_values" {
-    for_each = each.value.configuration != null && each.value.configuration != {} ? [1] : []
-    content {
-      configuration_values = jsonencode(each.value.configuration)
-    }
-  }
+  configuration_values = (
+    try(each.value.configuration, null) != null && try(each.value.configuration, {}) != {} ?
+    jsonencode(each.value.configuration) :
+    null
+  )
 }
