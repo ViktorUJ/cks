b11be092a4cf4d98ad06ff2056d7ed68eae49680
Viktar Mikalayeu <viktoruj@gmail.com>
2025-12-23 11:55:16 +0400

    ~

--- terraform/modules/eks_v2_control_plane/output.tf
+++ terraform/modules/eks_v2_control_plane/output.tf
@@ -2,5 +2,5 @@ output "kubectl_config" {
   value = "aws eks update-kubeconfig --region ${var.region} --name ${var.eks.name} "
 }
 output "eks_mudule_output" {
-  value = module.eks.outputs
+  value = module.eks
 }
