1038c42687646029b277f1dcce0b0954f0d6e054
Viktar Mikalayeu <viktoruj@gmail.com>
2025-12-26 20:31:23 +0400

    ~

--- terraform/modules/eks_v2_ebs_irsa/addon.tf
+++ terraform/modules/eks_v2_ebs_irsa/addon.tf
@@ -1,8 +1,8 @@
 
 resource "aws_eks_addon" "addons" {
   cluster_name      = var.name
-  addon_name        = var.name
-  addon_version     = var.addons.name
+  addon_name        = var.addons.name
+  addon_version     = var.addons.version
   resolve_conflicts_on_create = var.addons.resolve_conflicts
   resolve_conflicts_on_update=var.addons.resolve_conflicts
   service_account_role_arn = aws_iam_role.ebs_csi_irsa.arn
