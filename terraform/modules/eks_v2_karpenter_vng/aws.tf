aecfeec8c7c1d688035b47210fcd514d79b62c1c
Viktar Mikalayeu <viktoruj@gmail.com>
2025-12-27 11:56:57 +0400

    ~

--- terraform/modules/eks_v2_karpenter_vng/aws.tf
+++ terraform/modules/eks_v2_karpenter_vng/aws.tf
@@ -14,3 +14,9 @@ provider "helm" {
     token                  = data.aws_eks_cluster_auth.this.token
   }
 }
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.this.token
+}
