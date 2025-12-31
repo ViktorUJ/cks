4875dc0d5856c0d7666ca18eb7111d20aa909879
Viktar Mikalayeu <viktoruj@gmail.com>
2025-12-31 13:51:45 +0400

    ~

--- tasks/eks/labs/02/worker/files/worker.sh
+++ tasks/eks/labs/02/worker/files/worker.sh
@@ -1,4 +1,6 @@
-echo "*** eks worker"
+echo "*** eks worker lab 2 ***"
 wget https://raw.githubusercontent.com/ViktorUJ/cks/refs/heads/AG-122/tasks/eks/labs/02/worker/files/tasks/1.yaml
 cat 1.yaml
+sleep 20
+kubectl get ns
 kubectl  apply -f 1.yaml
