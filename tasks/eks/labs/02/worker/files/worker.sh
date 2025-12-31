51699e33d453d9434380519ec0c88aac23fe47bc
Viktar Mikalayeu <viktoruj@gmail.com>
2025-12-31 12:51:56 +0400

    ~

--- tasks/eks/labs/02/worker/files/worker.sh
+++ tasks/eks/labs/02/worker/files/worker.sh
@@ -1,3 +1,4 @@
 echo "*** eks worker"
-
-kubectl  apply -f  https://raw.githubusercontent.com/ViktorUJ/cks/refs/heads/AG-122/tasks/eks/labs/02/worker/files/tasks/1.yaml
+wget https://raw.githubusercontent.com/ViktorUJ/cks/refs/heads/AG-122/tasks/eks/labs/02/worker/files/tasks/1.yaml
+cat 1.yaml
+kubectl  apply -f 1.yaml
