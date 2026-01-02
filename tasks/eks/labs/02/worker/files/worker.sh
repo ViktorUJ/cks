e988f1faf217c3fe9ac0a69eefcbcc22ba7daf94
Viktar Mikalayeu <viktoruj@gmail.com>
2026-01-02 19:41:10 +0400

    ~

--- tasks/eks/labs/02/worker/files/worker.sh
+++ tasks/eks/labs/02/worker/files/worker.sh
@@ -1,5 +1,5 @@
 export KUBECONFIG=/root/.kube/config
 
 echo "*** eks worker lab 2 ***"
-kubectl  apply  https://raw.githubusercontent.com/ViktorUJ/cks/refs/heads/AG-122/tasks/eks/labs/02/worker/files/tasks/1.yaml
+kubectl  apply -f  https://raw.githubusercontent.com/ViktorUJ/cks/refs/heads/AG-122/tasks/eks/labs/02/worker/files/tasks/1.yaml
 
