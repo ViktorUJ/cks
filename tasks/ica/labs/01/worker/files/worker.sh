e63ad7db29165860888f0f4f22b85011c9839f56
Viktar Mikalayeu <viktoruj@gmail.com>
2026-03-14 13:23:53 +0400

    ~

--- tasks/ica/labs/01/worker/files/worker.sh
+++ tasks/ica/labs/01/worker/files/worker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo " *** worker pc cks lab 30 k8s-1"  "
+echo " *** worker pc ica lab 01 k8s-1"  "
 export KUBECONFIG=/root/.kube/config
 address=$(kubectl get no -o json  | jq -r '.items[] | select(.kind == "Node") | .status.addresses[] | select(.type == "InternalIP") | .address')
 echo "$address myapp.local">>/etc/hosts
