e6e267349a6eeee61fb64436ae43c96db59cffa9
Viktar Mikalayeu <viktoruj@gmail.com>
2026-08-11 23:30:22 +0400

    ~

--- terraform/modules/eks_v2_workload_identity_demo/secrets.tf
+++ terraform/modules/eks_v2_workload_identity_demo/secrets.tf
@@ -3,6 +3,12 @@
 resource "aws_secretsmanager_secret" "demo" {
   name = local.secret_name
   tags = merge(var.tags, { "Name" = local.secret_name })
+  # Имя секрета предсказуемое (prefix + app_name), лаба может быть удалена и развёрнута
+  # заново с тем же именем. По умолчанию AWS держит удалённый секрет в recovery window
+  # 7-30 дней ("scheduled for deletion"), и повторное create-secret с тем же именем
+  # падает с InvalidRequestException. recovery_window_in_days = 0 удаляет секрет сразу,
+  # без периода восстановления - для учебного стенда это уместно.
+  recovery_window_in_days = 0
 }
 
 resource "aws_secretsmanager_secret_version" "demo" {
